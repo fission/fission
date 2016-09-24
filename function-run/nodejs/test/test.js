5d76ee8894b0879931b927155914218be5b9dcf1
Soam Vasani <soamvasani@platform9.com>
2016-09-24 12:17:08 -0700

    Remove outdated comment

--- function-run/nodejs/test/test.js
+++ function-run/nodejs/test/test.js
@@ -2,7 +2,5 @@ module.exports = function (context, callback) {
     console.log("headers=", JSON.stringify(context.request.headers));
     console.log("body=", JSON.stringify(context.request.body));
 
-    //console.log(`Test function entered: ${context.request.body}`);
-
     callback(200, "Hello, world !\n");
 }
