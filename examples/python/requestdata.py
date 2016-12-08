4a6a088eca8710d33653126186f3ed7735195d49
Soam Vasani <soamvasani@platform9.com>
2016-12-08 14:13:01 -0800

    Modify example a bit

--- examples/python/requestdata.py
+++ examples/python/requestdata.py
@@ -2,7 +2,6 @@ from flask import request
 from flask import current_app
 
 def main():
-    msg = "%s %s:\n---HEADERS---\n%s\n--BODY--\n%s\n-----\n" % (request.method, request.path, request.headers, request.get_data())
-    current_app.logger.info(msg)
-
+    current_app.logger.info("Received request")
+    msg = "---HEADERS---\n%s\n--BODY--\n%s\n-----\n" % (request.headers, request.get_data())
     return msg
