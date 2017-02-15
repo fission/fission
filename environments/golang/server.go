da7989ed8208f301a62f18d028335e22e0d43de5
Valentin Tjoncke <valtjo@gmail.com>
2017-02-15 23:14:53 +0100

    gofmt golang env

--- environments/golang/server.go
+++ environments/golang/server.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
-  "plugin"
+	"fmt"
+	"net/http"
+	"os"
+	"plugin"
 )
 
 const (
-  CODE_PATH = "/userfunc/user"
+	CODE_PATH = "/userfunc/user"
 )
 
 var userFunc http.HandlerFunc
 
 func loadPlugin() http.HandlerFunc {
-  p, err := plugin.Open(CODE_PATH)
-  if err != nil {
-    panic(err)
-  }
-  sym, err := p.Lookup("HandlerFunc")
-  if err != nil {
-    panic("Entry point missing: func HandlerFunc(w http.ResponseWriter, r *http.Request)")
-  }
-  return sym.(func(http.ResponseWriter, *http.Request))
+	p, err := plugin.Open(CODE_PATH)
+	if err != nil {
+		panic(err)
+	}
+	sym, err := p.Lookup("HandlerFunc")
+	if err != nil {
+		panic("Entry point missing: func HandlerFunc(w http.ResponseWriter, r *http.Request)")
+	}
+	return sym.(func(http.ResponseWriter, *http.Request))
 }
 
 func specializeHandler(w http.ResponseWriter, r *http.Request) {
-  if userFunc != nil {
-    w.WriteHeader(400)
-    w.Write([]byte("Not a generic container"))
-    return
-  }
+	if userFunc != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("Not a generic container"))
+		return
+	}
 
-  _, err := os.Stat(CODE_PATH)
-  if err != nil {
-    if os.IsNotExist(err) {
-      w.WriteHeader(http.StatusNotFound)
-      w.Write([]byte(CODE_PATH + ": not found"))
-      return
-    } else {
-      panic(err)
-    }
-  }
+	_, err := os.Stat(CODE_PATH)
+	if err != nil {
+		if os.IsNotExist(err) {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(CODE_PATH + ": not found"))
+			return
+		} else {
+			panic(err)
+		}
+	}
 
-  fmt.Printf("Specializing... ")
-  userFunc = loadPlugin()
-  fmt.Println("Done")
+	fmt.Printf("Specializing... ")
+	userFunc = loadPlugin()
+	fmt.Println("Done")
 }
 
 func main() {
-  http.HandleFunc("/specialize", specializeHandler)
+	http.HandleFunc("/specialize", specializeHandler)
 
-  // Generic route -- all http requests go to the user function.
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    if userFunc == nil {
-      w.WriteHeader(http.StatusInternalServerError)
-      w.Write([]byte("Generic container: no requests supported"))
-      return
-    }
-    userFunc(w, r)
-  })
+	// Generic route -- all http requests go to the user function.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if userFunc == nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Generic container: no requests supported"))
+			return
+		}
+		userFunc(w, r)
+	})
 
-  fmt.Println("Listening on :8888 ...")
-  http.ListenAndServe(":8888", nil)
+	fmt.Println("Listening on :8888 ...")
+	http.ListenAndServe(":8888", nil)
 }
