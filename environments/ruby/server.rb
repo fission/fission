06a5c12a25f836479690b6f826ce4bb74990fcbb
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-07-04 10:17:25 -0400

    include path parameters in params hash for ruby environment

--- environments/ruby/server.rb
+++ environments/ruby/server.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 require 'rack'
 
-CODEPATH = '/userfunc/user'
-
 module Fission
+  CODE_PATH = '/userfunc/user'
+
+  HEADER_PREFIX = 'HTTP_'
+  PARAM_HEADER_PREFIX = 'HTTP_X_FISSION_PARAMS_'
+  PARAMETERS_KEY = 'fission.request.parameters'
+
   class Request < Rack::Request
     def headers
       Hash[
-        *env.select { |k,v| k.start_with?('HTTP_') }
-             .map { |k,v| [k.sub(/\AHTTP_/, '').split('_').map(&:capitalize).join('-'), v] }
+        *env.select { |k,v| k.start_with?(HEADER_PREFIX) }
+             .map { |k,v| [k.sub(/\A#{HEADER_PREFIX}/, '').split('_').map(&:capitalize).join('-'), v] }
+             .sort
+             .flatten
+      ]
+    end
+
+    def params
+      env[PARAMETERS_KEY] ||= super.merge(path_parameters)
+    end
+
+    def path_parameters
+      Hash[
+        *env.select { |k,v| k.start_with?(PARAM_HEADER_PREFIX) }
+             .map { |k,v| [k.sub(/\A#{PARAM_HEADER_PREFIX}/, '').downcase, v] }
              .sort
              .flatten
       ]
@@ -29,7 +46,7 @@ module Fission
 
   module Specializer
     def self.call(env)
-      load CODEPATH
+      load CODE_PATH
       Rack::Response.new([], 201).finish
     rescue
       Rack::Response.new(['500 Internal Server Error'], 500, {}).finish
