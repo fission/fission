873b7841813e340be2171c611f9a9e99acc2e2b2
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-07-05 23:28:45 -0400

    Add logger for ruby environment

--- environments/ruby/fission/handler.rb
+++ environments/ruby/fission/handler.rb
@@ -5,14 +5,18 @@ require_relative 'context'
 module Fission
   module Handler
     def self.call(env)
+      context = Context.new(env)
+
       response = if method(:handler).arity > 0
-        handler(Context.new(env))
+        handler(context)
       else
         handler
       end
 
       response.is_a?(Array) ? response : Rack::Response.new([response]).finish
-    rescue
+
+    rescue => e
+      context.logger.error(%(Function failed - #{e}\n#{e.backtrace.join("\n")}))
       Rack::Response.new(['500 Internal Server Error'], 500, {}).finish
     end
   end
--- environments/ruby/fission/specializer.rb
+++ environments/ruby/fission/specializer.rb
@@ -5,9 +5,14 @@ module Fission
 
   module Specializer
     def self.call(env)
+      request = Request.new(env)
+
       load CODE_PATH
+
       Rack::Response.new([], 201).finish
-    rescue
+
+    rescue => e
+      request.logger.error(%(Specialization failed - #{e}\n#{e.backtrace.join("\n")}))
       Rack::Response.new(['500 Internal Server Error'], 500, {}).finish
     end
   end
