5a6c9d9388bc9c7e15faa61059fdbd28d0b1e7ce
Soam Vasani <soamvasani@platform9.com>
2017-08-07 16:53:27 -0700

    Kubernetes access for Travis CI tests
    This change gets travis test runs to download and save kubectl, and to
    save a kubeconfig file from an environment variable.

---
+++ hack/travis-kube-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Download kubectl, save kubeconfig, and ensure we can access the test cluster
+#
+
+set -e 
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo $KUBECONFIG_CONTENTS | base64 -D - > ${HOME}/.kube/config
+
+kubectl version
