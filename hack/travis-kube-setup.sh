e387c9768cfa2c56359a714ea68abe9f82c9ebc5
Soam Vasani <soamvasani@platform9.com>
2017-08-08 14:01:05 -0700

    Use gcloud svc account

--- hack/travis-kube-setup.sh
+++ hack/travis-kube-setup.sh
@@ -11,6 +11,36 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 mkdir ${HOME}/.kube
-echo $KUBECONFIG_CONTENTS | base64 -D - > ${HOME}/.kube/config
 
-kubectl version
+# echo $KUBECONFIG_CONTENTS | base64 -D - > ${HOME}/.kube/config
+# kubectl version
+
+# gcloud stuff
+# https://stackoverflow.com/questions/38762590/how-to-install-google-cloud-sdk-on-travis
+
+if [ ! -d "${HOME}/google-cloud-sdk/bin" ]
+then
+    rm -rf $HOME/google-cloud-sdk
+    export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+    curl https://sdk.cloud.google.com | bash
+fi
+
+# gcloud command
+export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
+
+# ensure we have the gcloud binary
+gcloud version
+
+# get gcloud credentials
+echo $FISSION_CI_SERVICE_ACCOUNT | base64 -D - > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+# get kube config
+gcloud container clusters get-credentials fission-ci-1 --zone us-central1-a --project fission-ci
+
+# remove gcloud creds
+unset $FISSION_CI_SERVICE_ACCOUNT
+rm ${HOME}/gcloud-service-key.json
+
+# does it work?
+kubectl get node
