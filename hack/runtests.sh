8551805ab1db0253b259849b6da2ba1c46141a34
Soam Vasani <soamvasani@platform9.com>
2017-08-08 16:09:41 -0700

    bugfix

--- hack/runtests.sh
+++ hack/runtests.sh
@@ -3,8 +3,8 @@
 export NODE_IP=$(kubectl get node -o jsonpath='{$..addresses[?(@.type=="ExternalIP")].address}')
 
 # Hacks for poolmgr unit tests.
-export TEST_FETCHER_URL=$NODE_IP:30001
-export TEST_SPECIALIZE_URL=$NODE_IP:30002/specialize
+export TEST_FETCHER_URL=http://$NODE_IP:30001
+export TEST_SPECIALIZE_URL=http://$NODE_IP:30002/specialize
 
 go test -v -i $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go')
 go test -v $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go')
