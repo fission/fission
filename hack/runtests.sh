0ecc7121cb585588dcdaf55cf7af628b8d57c227
Soam Vasani <soamvasani@platform9.com>
2017-08-08 16:04:38 -0700

    Move tests to runtests.sh; further hacks for poolmgr tests

---
+++ hack/runtests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NODE_IP=$(kubectl get node -o jsonpath='{$..addresses[?(@.type=="ExternalIP")].address}')
+
+# Hacks for poolmgr unit tests.
+export TEST_FETCHER_URL=$NODE_IP:30001
+export TEST_SPECIALIZE_URL=$NODE_IP:30002/specialize
+
+go test -v -i $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go')
+go test -v $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go')
