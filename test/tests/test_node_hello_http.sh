3ee00d555ec3d0307a9350e647f880f9301599d5
Soam Vasani <soamvasani@platform9.com>
2017-08-21 14:31:14 -0700

    Pre-test cleanup

--- test/tests/test_node_hello_http.sh
+++ test/tests/test_node_hello_http.sh
@@ -7,18 +7,27 @@ ROOT=$(dirname $0)/../..
 fn=nodejs-hello-$(date +%N)
 
 # Create a hello world function in nodejs, test it with an http trigger
+echo "Pre-test cleanup"
+fission env delete --name nodejs || true
 
-echo "Test test, please ignore."
-
+echo "Creating nodejs env"
 fission env create --name nodejs --image fission/node-env
 trap "fission env delete --name nodejs" EXIT
 
+echo "Creating function"
 fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js
 trap "fission fn delete --name $fn" EXIT
 
+echo "Creating route"
 fission route create --function $fn --url /$fn --method GET
 
+echo "Waiting for router to catch up"
 sleep 3
 
+echo "Doing an HTTP GET on the function's route"
 response=$(curl http://$FISSION_ROUTER/$fn)
+
+echo "Checking for valid response"
 echo $response | grep -i hello
+
+echo "All done."
