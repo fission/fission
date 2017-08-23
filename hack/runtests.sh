e964a13a73a617f77787b0d3e9fa2179212380d9
Soam Vasani <soamvasani@platform9.com>
2017-08-23 00:02:42 -0700

    clean up cluster of all fission tprs before running tests

--- hack/runtests.sh
+++ hack/runtests.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# The poolmgr unit test only works with NodePort-type services for
-# now. So disable it for our travis ci tests.
 
 if [ ! -f ${KUBECONFIG} ]
 then
     unset KUBECONFIG
+else
+    K=kubectl --kubeconfig $KUBECONFIG
+    if $K get configmap ok-to-destroy
+    then
+	$K get function.fission.io -o name | cut -f2 -d'/' | xargs $K delete function.fission.io
+	$K get environment.fission.io -o name | cut -f2 -d'/' | xargs $K delete environment.fission.io
+	$K get httptrigger.fission.io -o name | cut -f2 -d'/' | xargs $K delete httptrigger.fission.io
+    fi
 fi
 
 go test -v -i $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go')
+
+# The poolmgr unit test only works with NodePort-type services for
+# now. So disable it for our travis ci tests.
 go test -v $(go list ./... | grep -v '/vendor/' | grep -v 'examples/go' | grep -v poolmgr)
