0381c675498aef79bd4a3b349e01c783aec07689
Ta-Ching Chen <contact@tachingchen.com>
2017-08-31 01:32:51 +0800

    Add builder client

--- environments/builder/client/client.go
+++ environments/builder/client/client.go
@@ -20,17 +20,29 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/fission/fission"
 )
 
-func DoBuildRequest(builderUrl string, br *fission.PackageBuildRequest) error {
-	body, err := json.Marshal(br)
+type (
+	BuilderClient struct {
+		Url string
+	}
+)
+
+func MakeBuilderClient(serverUrl string) *BuilderClient {
+	return &BuilderClient{
+		Url: strings.TrimSuffix(serverUrl, "/"),
+	}
+}
+
+func (br *BuilderClient) Build(req *fission.PackageBuildRequest) error {
+	body, err := json.Marshal(req)
 	if err != nil {
 		return err
 	}
-
-	resp, err := http.Post(builderUrl, "application/json", bytes.NewReader(body))
+	resp, err := http.Post(br.Url, "application/json", bytes.NewReader(body))
 	if err != nil {
 		return err
 	}
@@ -39,6 +51,5 @@ func DoBuildRequest(builderUrl string, br *fission.PackageBuildRequest) error {
 	if resp.StatusCode != 200 {
 		return fission.MakeErrorFromHTTP(resp)
 	}
-
 	return nil
 }
