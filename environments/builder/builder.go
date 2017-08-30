badcd4bfeb532103ad318f5efcd06a83b3d2a7a0
Ta-Ching Chen <contact@tachingchen.com>
2017-08-31 01:03:34 +0800

    Move archive & unarchive to fetcher

--- environments/builder/builder.go
+++ environments/builder/builder.go
@@ -27,19 +27,15 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/mholt/archiver"
+	"github.com/dchest/uniuri"
 
 	"github.com/fission/fission"
 )
 
 const (
-	pathPrefix      = "/tmp/fission"
-	srcPkgPath      = pathPrefix + "/srcPkg"
-	deployPkgPath   = pathPrefix + "/deployPkg"
-	archiveFilename = "archive.zip"
-
 	// supported environment variables
 	envSrcPkg    = "SRC_PKG"
 	envDeployPkg = "DEPLOY_PKG"
@@ -83,36 +79,21 @@ func (builder *Builder) Handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	log.Println("builder received request: %v", req)
-
-	log.Println("Uncompressing source package...")
-	srcPkgArchivePath := filepath.Join(builder.sharedVolumePath, req.SrcPkgFilename)
-	err = builder.unarchive(srcPkgArchivePath, srcPkgPath)
-	if err != nil {
-		e := errors.New(fmt.Sprintf("Failed to unarchive source package: %v", err))
-		http.Error(w, e.Error(), 500)
-		return
-	}
+	log.Printf("builder received request: %v", req)
 
 	log.Println("Starting build...")
-	buildLogs, err := builder.build(req.BuildCommand, srcPkgPath)
+	srcPkgPath := filepath.Join(builder.sharedVolumePath, req.SrcPkgFilename)
+	deployPkgFilename := fmt.Sprintf("%v-%v", req.SrcPkgFilename, strings.ToLower(uniuri.NewLen(6)))
+	deployPkgPath := filepath.Join(builder.sharedVolumePath, deployPkgFilename)
+	buildLogs, err := builder.build(req.BuildCommand, srcPkgPath, deployPkgPath)
 	if err != nil {
 		e := errors.New(fmt.Sprintf("Failed to build source package: %v", err))
 		http.Error(w, e.Error(), 500)
 		return
 	}
 
-	log.Println("Compressing deployment package...")
-	archivePath := filepath.Join(builder.sharedVolumePath, archiveFilename)
-	err = builder.archive(deployPkgPath, archivePath)
-	if err != nil {
-		e := errors.New(fmt.Sprintf("Failed to archive deployment package: %v", err))
-		http.Error(w, e.Error(), 500)
-		return
-	}
-
 	resp := fission.PackageBuildResponse{
-		ArchiveFilename: archiveFilename,
+		ArchiveFilename: deployPkgFilename,
 		BuildLogs:       buildLogs,
 	}
 
@@ -128,9 +109,9 @@ func (builder *Builder) Handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (builder *Builder) build(command string, workDir string) (string, error) {
+func (builder *Builder) build(command string, srcPkgPath string, deployPkgPath string) (string, error) {
 	cmd := exec.Command(command)
-	cmd.Dir = workDir
+	cmd.Dir = srcPkgPath
 	// set env variables for build command
 	cmd.Env = append(os.Environ(),
 		fmt.Sprintf("%v=%v", envSrcPkg, srcPkgPath),
@@ -151,12 +132,13 @@ func (builder *Builder) build(command string, workDir string) (string, error) {
 
 	var buildLogs string
 
-	fmt.Printf("\n=== Build Logs ===\n")
+	fmt.Println("\n=== Build Logs ===")
 	for scanner.Scan() {
 		output := scanner.Text()
 		fmt.Println(output)
 		buildLogs += fmt.Sprintf("%v\n", output)
 	}
+	fmt.Println("==================\n")
 
 	if err := scanner.Err(); err != nil {
 		return "", errors.New(fmt.Sprintf("Error reading cmd output: %v", err.Error()))
@@ -168,32 +150,4 @@ func (builder *Builder) build(command string, workDir string) (string, error) {
 	}
 
 	return buildLogs, nil
-}
-
-// archive is a function that zips directory into a zip file
-func (builder *Builder) archive(src string, dst string) error {
-	var files []string
-	target, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-	if target.IsDir() {
-		// list all
-		fs, _ := ioutil.ReadDir(src)
-		for _, f := range fs {
-			files = append(files, filepath.Join(deployPkgPath, f.Name()))
-		}
-	} else {
-		files = append(files, src)
-	}
-	return archiver.Zip.Make(dst, files)
-}
-
-// unarchive is a function that unzips a zip file to destination
-func (builder *Builder) unarchive(src string, dst string) error {
-	err := archiver.Zip.Open(src, dst)
-	if err != nil {
-		return errors.New(fmt.Sprintf("Failed to unzip source package: %v", err))
-	}
-	return nil
 }
