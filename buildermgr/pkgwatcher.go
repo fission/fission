954d371536c8de6976eb7441a2ae4aa2072d394f
Ta-Ching Chen <contact@tachingchen.com>
2017-09-14 02:19:29 +0800

    Add pkgwatcher

---
+++ buildermgr/pkgwatcher.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2017 The Fission Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package buildermgr
+
+import (
+	"log"
+	"time"
+
+	"k8s.io/client-go/1.5/kubernetes"
+	"k8s.io/client-go/1.5/pkg/api"
+	"k8s.io/client-go/1.5/pkg/watch"
+
+	"github.com/fission/fission"
+	"github.com/fission/fission/tpr"
+)
+
+const (
+	BUILD = iota
+)
+
+type (
+	packageWatcher struct {
+		fissionClient    *tpr.FissionClient
+		kubernetesClient *kubernetes.Clientset
+		builderNamespace string
+		storageSvcUrl    string
+	}
+)
+
+func makePackageWatcher(fissionClient *tpr.FissionClient,
+	kubernetesClient *kubernetes.Clientset, builderNamespace string, storageSvcUrl string) *packageWatcher {
+	pkgw := &packageWatcher{
+		fissionClient:    fissionClient,
+		kubernetesClient: kubernetesClient,
+		builderNamespace: builderNamespace,
+		storageSvcUrl:    storageSvcUrl,
+	}
+	go pkgw.service()
+	return pkgw
+}
+
+func (pkgw *packageWatcher) service() {
+}
+
+func (pkgw *packageWatcher) build(pkgMetadata api.ObjectMeta) {
+	buildReq := BuildRequest{
+		Package: pkgMetadata,
+	}
+	_, _, err := buildPackage(pkgw.fissionClient,
+		pkgw.kubernetesClient, pkgw.builderNamespace, pkgw.storageSvcUrl, buildReq)
+	if err != nil {
+		log.Printf("Error building package %v: %v", buildReq.Package.Name, err)
+	}
+}
+
+func (pkgw *packageWatcher) watchPackages() {
+	rv := ""
+	for {
+		wi, err := pkgw.fissionClient.Packages(api.NamespaceDefault).Watch(api.ListOptions{
+			ResourceVersion: rv,
+		})
+		if err != nil {
+			log.Fatalf("Error watching package TPR resources: %v", err)
+		}
+
+		for {
+			log.Println("watching")
+			ev, more := <-wi.ResultChan()
+			if !more {
+				break
+			}
+			if ev.Type == watch.Error {
+				rv = ""
+				time.Sleep(time.Second)
+				break
+			}
+			pkg := ev.Object.(*tpr.Package)
+			rv = pkg.Metadata.ResourceVersion
+
+			// only do build for packages in pending state
+			if pkg.Spec.Status.BuildStatus == fission.BuildStatusPending {
+				go pkgw.build(pkg.Metadata)
+			}
+		}
+	}
+}
