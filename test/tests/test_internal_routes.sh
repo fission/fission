1ab7ad92853e6b3cbbf514f310f566d7bab636fe
Soam Vasani <soamvasani@platform9.com>
2017-09-19 23:36:15 -0700

    Add test for internal routes

---
+++ test/tests/test_internal_routes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ROOT=$(dirname $0)/../..
+
+
+# Create a hello world function in nodejs, test it with an http trigger
+echo "Pre-test cleanup"
+fission env delete --name nodejs || true
+
+echo "Creating nodejs env"
+fission env create --name nodejs --image fission/node-env
+trap "fission env delete --name nodejs" EXIT
+
+echo "Writing functions"
+f1=f1-$(date +%s)
+f2=f2-$(date +%s)
+echo $f1 $f2
+
+for f in $f1 $f2
+do
+    echo "module.exports = function(context, callback) { callback(200, \"$f\n\"); }" > $f.js
+done
+
+echo "Creating functions"
+for f in $f1 $f2
+do
+    fission fn create --name $f --env nodejs --code $f.js
+    trap "fission fn delete --name $f" EXIT
+done
+
+echo "Waiting for router to catch up"
+sleep 3
+
+echo "Testing internal routes"
+for f in $f1 $f2
+do
+    response=$(curl http://$FISSION_ROUTER/fission-function/$f)
+    echo $response | grep $f
+done
+
+echo "All done."
