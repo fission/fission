7bd3f20ad671f069bf1c484191f4c9e3f455ef70
João Almeida <joao.diogo.almeida@gmail.com>
2017-09-21 11:30:15 -0700

    Fission dotnet 2.0 env (#337)
    Adding a new dotnet environment, supporting dotnet core 2.0.
    
    Project structure changed. Project.json was abandoned. Project.csproj is now used.

---
+++ examples/dotnet20/arguments.cs
@@ -0,0 +1,11 @@
+using System;
+using Fission.DotNetCore.Api;
+
+public class FissionFunction 
+{
+    public string Execute(FissionContext context){
+        var x = Convert.ToInt32(context.Arguments["x"]);
+        var y = Convert.ToInt32(context.Arguments["y"]);
+        return (x+y).ToString();
+    }
+
