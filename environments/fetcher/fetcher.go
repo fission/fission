2260713f6fd9cdf4b306fe84d4de4da76906de88
Ta-Ching Chen <contact@tachingchen.com>
2017-10-24 11:24:13 +0800

    Revert "Archive content of source package"
    This reverts commit 21f7f1cf2907fe372e93caccbe1be4e93b6eabf0.

--- environments/fetcher/fetcher.go
+++ environments/fetcher/fetcher.go
@@ -329,10 +329,21 @@ func (fetcher *Fetcher) rename(src string, dst string) error {
 
 // archive is a function that zips directory into a zip file
 func (fetcher *Fetcher) archive(src string, dst string) error {
-	if _, err := os.Stat(src); err != nil {
+	var files []string
+	target, err := os.Stat(src)
+	if err != nil {
 		return errors.New(fmt.Sprintf("Failed to zip file: %v", err))
 	}
-	return archiver.Zip.Make(dst, []string{src})
+	if target.IsDir() {
+		// list all
+		fs, _ := ioutil.ReadDir(src)
+		for _, f := range fs {
+			files = append(files, filepath.Join(src, f.Name()))
+		}
+	} else {
+		files = append(files, src)
+	}
+	return archiver.Zip.Make(dst, files)
 }
 
 // unarchive is a function that unzips a zip file to destination
