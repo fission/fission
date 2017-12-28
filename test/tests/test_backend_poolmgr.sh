103f86c023489b6573d62f1416dd869cb2cee813
Vishal Biyani <vishal@infracloud.io>
2017-12-28 16:19:30 +0530

    Adding tests for poolmgr backend

--- test/tests/test_backend_poolmgr.sh
+++ test/tests/test_backend_poolmgr.sh
@@ -7,7 +7,7 @@ ROOT=$(dirname $0)/../..
 fn=nodejs-hello-$(date +%N)
 
 # Create a hello world function in nodejs, test it with an http trigger
-echo "NewDeploy Backend: Pre-test cleanup"
+echo "Poolmgr Backend: Pre-test cleanup"
 fission env delete --name nodejs || true
 
 echo "Creating nodejs env"
@@ -15,13 +15,13 @@ fission env create --name nodejs --image fission/node-env --mincpu 20 --maxcpu 1
 trap "fission env delete --name nodejs" EXIT
 
 echo "Creating function"
-fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js --minscale 2 --maxscale 4 --backend newdeploy --eagercreate
+fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js --backend poolmgr
 trap "fission fn delete --name $fn" EXIT
 
 echo "Creating route"
 fission route create --function $fn --url /$fn --method GET
 
-echo "Waiting for router & newdeploy deployment creation"
+echo "Waiting for router to catch up"
 sleep 5
 
 echo "Doing an HTTP GET on the function's route"
@@ -33,4 +33,4 @@ echo $response | grep -i hello
 # crappy cleanup, improve this later
 kubectl get httptrigger -o name | tail -1 | cut -f2 -d'/' | xargs kubectl delete httptrigger
 
-echo "NewDeploy Backend: All done."
+echo "Poolmgr Backend: All done."
