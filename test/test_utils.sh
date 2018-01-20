6a1da19e7364838b5b1ac0395b6e4af622a37e17
Ta-Ching Chen <contact@tachingchen.com>
2018-01-20 22:06:50 +0800

    Fix python environment failed to launch due to port needs integer not string (#451)

--- test/test_utils.sh
+++ test/test_utils.sh
@@ -167,10 +167,10 @@ helm_install_fission() {
 
     helmVars=image=$image,imageTag=$imageTag,fetcherImage=$fetcherImage,fetcherImageTag=$fetcherImageTag,functionNamespace=$fns,controllerPort=$controllerNodeport,routerPort=$routerNodeport,pullPolicy=Always,analytics=false,logger.fluentdImage=$fluentdImage
 
-    timeout 30 helm_setup
+    timeout 30 bash -c "helm_setup"
 
     echo "Deleting old releases"
-    helm list -q|xargs helm_uninstall_fission
+    helm list -q|xargs -I@ bash -c "helm_uninstall_fission @"
 
     echo "Installing fission"
     helm install		\
