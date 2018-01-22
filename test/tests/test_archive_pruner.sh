2f7ce9bc49b7c018094e07b580ae23f62c8a1356
smruthi2187 <smruthi@platform9.com>
2018-01-22 15:27:27 -0800

    Adding helm value pruneInterval for testing.

--- test/tests/test_archive_pruner.sh
+++ test/tests/test_archive_pruner.sh
@@ -11,10 +11,10 @@ set -euo pipefail
 #7. now verify that both get deleted.
 #8. delete huge file, zip files
 
-pkg_1 = ""
-pkg_2 = ""
+pkg_1=""
+pkg_2=""
 create_packages() {
-    echo "Creating a package with deploy archive"
+    echo "Creating 2 packages with deploy archives"
     mkdir test_dir
     touch test_dir/__init__.py
     if [ `uname` == "Darwin" ] ; then
@@ -34,7 +34,6 @@ create_packages() {
                 pkg_2=$pkg
         fi
     done
-    echo "Packages created : $pkg_1 $pkg_2"
     rm -rf test_dir test-deploy-pkg*
 }
 
@@ -50,24 +49,33 @@ delete_packages() {
     fi
 }
 
-http_status = ""
-get_archive_from_storage() {
-    # TODO : need to use fission cli to get HTTP status
-    url = `kubectl get $1 -ojsonpath='{.spec.deployment.url}'`
-    echo "url : $url"
-    http_status = `curl $url -sI -XGET | grep -i "http" | cut -d" " -f2`
+url=""
+get_archive_url_from_package() {
+    url=`kubectl get package $1 -ojsonpath='{.spec.deployment.url}'`
 }
 
-main () {
+http_status=""
+get_archive_from_storage() {
+    # TODO : need to use fission cli to get HTTP status
+    http_status=`curl $1 -sI -XGET | grep -i "http" | cut -d" " -f2`
+}
+
+main() {
     create_packages
     echo "created packages : $pkg_1 $pkg_2"
 
-    sleep 30
+    get_archive_url_from_package $pkg_1
+    url_1=$url
+    echo "url1 : $url_1"
 
-    delete_packages
-    echo "deleted packages : $pkg_1 $pkg_2"
+    get_archive_url_from_package $pkg_2
+    url_2=$url
+    echo "url2 : $url_2"
 
-    get_archive_from_storage $pkg_1
+#    delete_packages
+#    echo "deleted packages : $pkg_1 $pkg_2"
+
+    get_archive_from_storage $url_1
     echo "http_status : $http_status"
     if [ "$http_status" -ne "200" ]; then
         echo "Something went wrong. Test failed."
@@ -75,7 +83,7 @@ main () {
         exit 1
     fi
 
-    get_archive_from_storage $pkg_2
+    get_archive_from_storage $url_2
     echo "http_status : $http_status"
     if [ "$http_status" -ne "200" ]; then
         echo "Something went wrong. Test failed."
@@ -83,24 +91,25 @@ main () {
         exit 1
     fi
 
-    # give it some time to get recycled
-    sleep 120
-
-    get_archive_from_storage $pkg_1
-    echo "http_status : $http_status"
-    if [ "$http_status" -ne "404" ]; then
-        echo "Something went wrong. Test failed."
-        cleanup
-        exit 1
-    fi
-
-    get_archive_from_storage $pkg_2
-    echo "http_status : $http_status"
-    if [ "$http_status" -ne "404" ]; then
-        echo "Something went wrong. Test failed."
-        cleanup
-        exit 1
-    fi
+    # give it some time to get recycled by the archivePruner
+#    echo "waiting for packages to get recycled"
+#    sleep 120
+#
+#    get_archive_from_storage $url_1
+#    echo "http_status : $http_status"
+#    if [ "$http_status" -ne "404" ]; then
+#        echo "Something went wrong. Test failed."
+#        cleanup
+#        exit 1
+#    fi
+#
+#    get_archive_from_storage $url_2
+#    echo "http_status : $http_status"
+#    if [ "$http_status" -ne "404" ]; then
+#        echo "Something went wrong. Test failed."
+#        cleanup
+#        exit 1
+#    fi
 
     echo "Test archive pruner PASSED"
 }
