99253245aab12207a1a42353abda2aa34c984c07
smruthi2187 <smruthi@platform9.com>
2018-01-22 22:01:39 -0800

    Modified test.

--- test/tests/test_archive_pruner.sh
+++ test/tests/test_archive_pruner.sh
@@ -2,21 +2,30 @@
 
 set -euo pipefail
 
-#1. create 2 archives with large files such that total size of archive is > 256KB
-#2. create 2 pkgs referencing those archives
-#3. delete both the packages
-#4. sleep for 30 seconds
-#5. verify archives are not recycled . this handles the case where archives are just created but not referenced by pkgs yet.
-#6. sleep for one minute
-#7. now verify that both get deleted.
-#8. delete huge file, zip files
+# global variables
+pkg=""
+http_status=""
+url=""
+kubectl_pf_pid=""
 
-pkg_1=""
-pkg_2=""
-create_packages() {
-    echo "Creating 2 packages with deploy archives"
+cleanup() {
+    rm -rf test-deploy-pkg.zip test_dir /tmp/file
+    if [ "$kubectl_pf_pid" != "" ]; then
+        kill -9 $kubectl_pf_pid
+    fi
+}
+
+check_return_status() {
+    if [ $1 -ne 0 ]; then
+        echo "Error"
+        cleanup
+        exit 1
+    fi
+}
+
+create_archive() {
+    echo "Creating an archive"
     mkdir test_dir
-    touch test_dir/__init__.py
     if [ `uname` == "Darwin" ] ; then
         mkfile 512m test_dir/dynamically_generated_file
     else
@@ -24,94 +33,123 @@ create_packages() {
         truncate -s 512M test_dir/dynamically_generated_file
     fi
     printf 'def main():\n    return "Hello, world!"' > test_dir/hello.py
-    for (( c=1; c<=2; c++ ))
-    do
-        zip -jr test-deploy-pkg-$c.zip test_dir/
-        pkg=$(fission package create --deploy "test-deploy-pkg-$c.zip" --env python| cut -f2 -d' '| tr -d \')
-        if [ $c == 1 ]; then
-                pkg_1=$pkg
-        else
-                pkg_2=$pkg
-        fi
-    done
-    rm -rf test_dir test-deploy-pkg*
+    zip -jr test-deploy-pkg.zip test_dir/
+    check_return_status $?
 }
 
-delete_packages() {
-    echo "Deleting packages: $pkg_1 $pkg_2"
-    fission package delete --name $pkg_1
-    if [ $? -ne 0 ]; then
-        echo "Error deleting package: $pkg_1"
-    fi
-    fission package delete --name $pkg_2
-    if [ $? -ne 0 ]; then
-        echo "Error deleting package: $pkg_2"
-    fi
+create_package() {
+    echo "Creating package"
+    pkg=$(fission package create --deploy "test-deploy-pkg.zip" --env python| cut -f2 -d' '| tr -d \')
+    check_return_status $?
 }
 
-url=""
+delete_package() {
+    echo "Deleting package: $1"
+    fission package delete --name $1
+    check_return_status $?
+}
+
 get_archive_url_from_package() {
+    echo "Getting archive URL from package: $1"
     url=`kubectl get package $1 -ojsonpath='{.spec.deployment.url}'`
+    check_return_status $?
 }
 
-http_status=""
+
+kubectl_port_forward() {
+    controller_pod=`kubectl get pods --all-namespaces | grep "controller" | tr -s ' '| cut -d" " -f2`
+    controller_ns=`kubectl get pods --all-namespaces | grep "controller" | tr -s ' '| cut -d" " -f1`
+    remote_port=`kubect get svc controller -n $controller_ns -ojsonpath='{.spec.ports.nodePort}'`
+    local_port=35565
+    echo "controller pod : $controller_pod, controller_ns: $controller_ns, remote_port : $remote_port"
+    kubectl port-forward $controller_pod $local_port:$remote_port &
+    kubectl_pf_pid=$!
+    echo "kubectl port forward process id : $kubectl_pf_pid"
+}
+
 get_archive_from_storage() {
-    # TODO : need to use fission cli to get HTTP status
-    http_status=`curl $1 -sI -XGET | grep -i "http" | cut -d" " -f2`
+    http_status=`curl -sw "%{http_code}" $1 -o /tmp/file`
 }
 
+#1. create an archives with large files such that total size of archive is > 256KB
+#2. create 2 pkgs referencing those archives
+#3. delete both the packages
+#4. sleep for 30 seconds
+#5. verify archives are not recycled . this handles the case where archives are just created but not referenced by pkgs yet.
+#6. sleep for one minute
+#7. now verify that both get deleted.
+#8. cleanup
 main() {
-    create_packages
-    echo "created packages : $pkg_1 $pkg_2"
+    # create a huge archive
+    create_archive
+    echo "created archive test-deploy-pkg.zip"
 
+    # create packages with the huge archive
+    create_package
+    pkg_1=$pkg
     get_archive_url_from_package $pkg_1
     url_1=$url
-    echo "url1 : $url_1"
+    echo "pkg: $pkg_1, archive_url : $url_1"
 
+    create_package
+    pkg_2=$pkg
     get_archive_url_from_package $pkg_2
     url_2=$url
-    echo "url2 : $url_2"
+    echo "pkg: $pkg_2, archive_url : $url_2"
 
-#    delete_packages
-#    echo "deleted packages : $pkg_1 $pkg_2"
+    # delete packages
+    delete_package $pkg_1
+    delete_package $pkg_2
+    echo "deleted packages : $pkg_1 $pkg_2"
 
+    # to find out if archive is present or absent on the storage, we can curl the archive url
+    # very soon, the controller (proxying for storage http requests) will not have a public IP.
+    # so, in any case, do a port forward of controller pod before executing curl get of archive url.
+    kubectl_port_forward
+
+    # curl on the archive url
     get_archive_from_storage $url_1
     echo "http_status : $http_status"
     if [ "$http_status" -ne "200" ]; then
-        echo "Something went wrong. Test failed."
+        echo "Archive $url_1 absent on storage, while expected to be present"
         cleanup
         exit 1
     fi
 
+    # curl on the archive url
     get_archive_from_storage $url_2
     echo "http_status : $http_status"
     if [ "$http_status" -ne "200" ]; then
-        echo "Something went wrong. Test failed."
+        echo "Archive $url_2 absent on storage, while expected to be present"
         cleanup
         exit 1
     fi
 
     # give it some time to get recycled by the archivePruner
-#    echo "waiting for packages to get recycled"
-#    sleep 120
-#
-#    get_archive_from_storage $url_1
-#    echo "http_status : $http_status"
-#    if [ "$http_status" -ne "404" ]; then
-#        echo "Something went wrong. Test failed."
-#        cleanup
-#        exit 1
-#    fi
-#
-#    get_archive_from_storage $url_2
-#    echo "http_status : $http_status"
-#    if [ "$http_status" -ne "404" ]; then
-#        echo "Something went wrong. Test failed."
-#        cleanup
-#        exit 1
-#    fi
+    # archivePruner is set to run every minute for test. In production, its set to run every hour.
+    echo "waiting for packages to get recycled"
+    sleep 120
+
+    # curl on the archive url
+    get_archive_from_storage $url_1
+    echo "http_status : $http_status"
+    if [ "$http_status" -ne "404" ]; then
+        echo "Archive $url_1 should have been recycled, but curl returned $http_status, while expected status is 404."
+        cleanup
+        exit 1
+    fi
+
+    # curl on the archive url
+    get_archive_from_storage $url_2
+    echo "http_status : $http_status"
+    if [ "$http_status" -ne "404" ]; then
+        echo "Archive $url_2 should have been recycled, but curl returned $http_status, while expected status is 404."
+        cleanup
+        exit 1
+    fi
 
     echo "Test archive pruner PASSED"
+    cleanup
 }
 
 main
