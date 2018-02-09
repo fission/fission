d9b5c2e560e2440727d6350096f468c9f1ca8870
Soam Vasani <soamvasani@platform9.com>
2018-02-08 18:55:49 -0800

    Go builder for single file functions (#492)
    Add support to go builder for building a single file (otherwise even a single file needs to be archived in a .zip)

--- environments/go/builder/build.sh
+++ environments/go/builder/build.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-cd ${SRC_PKG}
-go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
+if [ -d ${SRC_PKG} ]
+then
+    echo "Building in directory ${SRC_PKG}"
+    cd ${SRC_PKG}
+    go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
+elif [ -f ${SRC_PKG} ]
+then
+    fn=$(basename ${SRC_PKG})
+    d=/tmp/$fn
+    mkdir $d
+    trap "rm -rf /tmp/$fn" EXIT
+    echo "Building file ${SRC_PKG} in $d"
+
+    cd $d
+    cp ${SRC_PKG} ./function.go
+    go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
+fi
