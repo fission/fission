f695a81c46b1e61421d87e0504543419360d06f7
Ta-Ching Chen <contact@tachingchen.com>
2018-02-13 07:42:23 +0800

    Improve release script (#494)
    * Add step for releasing environment builder image
    
    * Strip $GOPATH from strack trace

--- builder/cmd/build.sh
+++ builder/cmd/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o builder .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -o builder .
 
--- environments/fetcher/cmd/build.sh
+++ environments/fetcher/cmd/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o fetcher .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -o fetcher .
