056f62c26bec37b489c5bab84815a61bbc2d7458
Vishal Biyani <vishal@infracloud.io>
2018-05-11 21:40:40 +0530

    Removing console out added for debugging

--- fission/environment.go
+++ fission/environment.go
@@ -95,7 +95,6 @@ func envCreate(c *cli.Context) error {
 	extractArchive := true
 	if c.IsSet("extract") {
 		extractArchive = c.Bool("extract")
-		fmt.Println("extractArchive=", extractArchive)
 	}
 
 	// Environment API interface version is not specified and
