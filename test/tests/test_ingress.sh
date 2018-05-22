654b22dbc6c750852e624e4c4c079acc1e19fad5
Vishal Biyani <vishal@infracloud.io>
2018-05-22 15:29:50 +0530

    Added test and a owner reference to the ingress created by the HttpTrigger

---
+++ test/tests/test_ingress.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+relativeUrl="/itest"
+functionName="hellojs"
+hostName="test.com"
+
+cleanup() {
+    fission route delete --name $1
+}
+
+log "Creating route for URL $relativeUrl"
+route_name = $(fission route create --url $relativeUrl --function $functionName --createingress| grep trigger| cut -d" " -f 2|cut -d"'" -f 2)
+trap "cleanup $route_name" EXIT
+
+log "Verifying to route in ingress"
+actual_route = $(k -n fission get ing -l 'functionName=$functionName,triggerName=$route_name' -o=jsonpath='{.items[0].spec.rules[0].http.paths[0].path}')
+
+if [ $actual_route != $relativeUrl ]
+then
+    log "Provided route and route in ingress don't match"
+    exit 1
+fi
+
+log "Modifying the route by adding host"
+fission route update --name $route_name --host $hostName --function $functionName
+
+actual_host=$(k -n fission get ing -l 'functionName=$functionName,triggerName=$route_name' -o=jsonpath='{.items[0].spec.rules[0].host}')
+
+if [ $hostName != $actual_host ]
+then
+    log "Provided host and host in ingress don't match"
+    exit 1
+fi
