72087891a0f07256ca4838e6d9d119b20a92d54d
Vishal Biyani <vishal@infracloud.io>
2018-05-23 17:48:17 +0530

    Getting ing from all namespaces as the namespace for fission pods changes every time

--- test/tests/test_ingress.sh
+++ test/tests/test_ingress.sh
@@ -8,20 +8,8 @@ functionName="hellotest"
 hostName="test.com"
 
 cleanup() {
-    fission env delete --name nodejs
-    fission fn delete --name $functionName
     fission route delete --name $1
 }
-
-log "Pre-test cleanup"
-fission env delete --name nodejs || true
-
-log "Creating nodejs env"
-fission env create --name nodejs --image fission/node-env
-
-
-log "Creating function"
-fission fn create --name $functionName --env nodejs --code $ROOT/examples/nodejs/hello.js
 
 log "Creating route for URL $relativeUrl"
 route_name=$(fission route create --url $relativeUrl --function $functionName --createingress| grep trigger| cut -d" " -f 2|cut -d"'" -f 2)
@@ -33,9 +21,9 @@ sleep 5
 
 log "Verifying to route in ingress"
 
-kubectl -n fission get ing -l 'functionName='$functionName',triggerName='$route_name -o=json
+kubectl get ing -l 'functionName='$functionName',triggerName='$route_name --all-namespaces -o=json
 
-actual_route=$(kubectl -n fission get ing -l 'functionName='$functionName',triggerName='$route_name -o=jsonpath='{.items[0].spec.rules[0].http.paths[0].path}')
+actual_route=$(kubectl -n fission get ing -l 'functionName='$functionName',triggerName='$route_name --all-namespaces -o=jsonpath='{.items[0].spec.rules[0].http.paths[0].path}')
 
 if [ $actual_route != $relativeUrl ]
 then
@@ -48,7 +36,7 @@ fission route update --name $route_name --host $hostName --function $functionNam
 
 sleep 2
 
-actual_host=$(kubectl -n fission get ing -l 'functionName='$functionName',triggerName='$route_name -o=jsonpath='{.items[0].spec.rules[0].host}')
+actual_host=$(kubectl get ing -l 'functionName='$functionName',triggerName='$route_name --all-namespaces -o=jsonpath='{.items[0].spec.rules[0].host}')
 
 if [ $hostName != $actual_host ]
 then
