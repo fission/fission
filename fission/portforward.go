2b21a1e8307581cee79f194580673ec7a0174c40
Joe Stevens <thejosephstevens@gmail.com>
2018-05-31 10:10:55 -0700

    comment cleanup

--- fission/portforward.go
+++ fission/portforward.go
@@ -22,20 +22,11 @@ func findFreePort() (string, error) {
 	}
 
 	port := strconv.Itoa(listener.Addr().(*net.TCPAddr).Port)
-	/*file, err := listener.(*net.TCPListener).File()
-	if err != nil {
-		return "", nil
-	}*/
 
 	err = listener.Close()
 	if err != nil {
 		return "", err
 	}
-
-	/*err = file.Close()
-	if err != nil {
-		return "", err
-	}*/
 
 	return port, nil
 }
