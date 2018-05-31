1fbbceafb9e267aa822bf91770f0c785b8a20f0b
Joe Stevens <thejosephstevens@gmail.com>
2018-05-31 13:40:57 -0700

    fix the cli install script for windows

--- fission/install-windows-cli.ps1
+++ fission/install-windows-cli.ps1
@@ -1,3 +1,5 @@
 go install
-rm $Env:GOPATH\bin\fission-test.exe
+if (Test-Path $Env:GOPATH\bin\fission-test.exe -PathType Leaf) {
+    rm $Env:GOPATH\bin\fission-test.exe
+}
 mv $Env:GOPATH\bin\fission.exe $Env:GOPATH\bin\fission-test.exe
