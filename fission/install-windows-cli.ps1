2db61e8313f00364a8d5fd2059715fcb7b8819fe
Joe Stevens <thejosephstevens@gmail.com>
2018-06-04 13:59:03 -0700

    removing unnecessary script

--- fission/install-windows-cli.ps1
+++
@@ -1,5 +0,0 @@
-go install
-if (Test-Path $Env:GOPATH\bin\fission-test.exe -PathType Leaf) {
-    rm $Env:GOPATH\bin\fission-test.exe
-}
-mv $Env:GOPATH\bin\fission.exe $Env:GOPATH\bin\fission-test.exe
