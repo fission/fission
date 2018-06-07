952b48bd68eb72e21702e4e39d8b107456426d47
Soam Vasani <soamvasani@platform9.com>
2018-06-07 14:17:27 -0700

    Environment warning message bugfix (#725)

--- fission/environment.go
+++ fission/environment.go
@@ -57,9 +57,7 @@ func envCreate(c *cli.Context) error {
 
 	envList, err := client.EnvironmentList(envNamespace)
 	if err == nil && len(envList) > 0 {
-		warn(fmt.Sprintf("%d environment(s) are present in this ns: %s. All these envs share"+
-			" the same service account token, with previleges to view secrets of all the functions referencing them. "+
-			"Envs can be created in different ns if isolation is needed", len(envList), envNamespace))
+		verbose(2, "%d environment(s) are present in the %s namespace.  These environments are not isolated from each other; use separate namespaces if you need isolation.", len(envList), envNamespace)
 	}
 
 	var poolsize int
