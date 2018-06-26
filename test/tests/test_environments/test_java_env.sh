20d9dc81099605285abdeaefb4beff3e292f28d7
Vishal Biyani <vishal@infracloud.io>
2018-06-26 18:34:13 +0530

    Added test for Java environment and related files

---
+++ test/tests/test_environments/test_java_env.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ROOT=$(dirname $0)/../../..
+
+cleanup() {
+    fission fn delete --name hellon
+    fission fn delete --name hellop
+    fission env delete --name jvm
+}
+
+test_fn() {
+    echo "Doing an HTTP GET on the function's route"
+    echo "Checking for valid response"
+
+    while true; do
+      response0=$(curl http://$FISSION_ROUTER/fission-function/$1)
+      echo $response0 | grep -i $2
+      if [[ $? -eq 0 ]]; then
+        break
+      fi
+      sleep 1
+    done
+}
+export -f test_fn
+
+cd $ROOT/examples/jvm/java
+
+log "Creating the jar from application"
+#Using Docker to build Jar so that maven & other Java dependencies are not needed on CI server
+docker run --name javabuilder javaex 
+docker cp javabuilder:/usr/src/myapp/target/hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar .
+docker rm -f javabuilder
+mv hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
+
+log "Creating environment for Java"
+fission env create --name jvm --image fission/jvm-env --version 2 --extract=false
+
+log "Creating pool manager & new deployment function for Java"
+fission fn create --name hellop --deploy app.jar --env jvm --entrypoint io.fission.HelloWorld
+fission fn create --name hellon --deploy app.jar --env jvm --executortype newdeploy --entrypoint io.fission.HelloWorld
+trap cleanup EXIT
+
+log "Waiting for router & pools to catch up"
+sleep 5
+
+log "Testing pool manager function"
+timeout 60 bash -c "test_fn hellop 'Hello World'"
+
+log "Testing new deployment function"
+timeout 60 bash -c "test_fn hellon 'Hello World'"
