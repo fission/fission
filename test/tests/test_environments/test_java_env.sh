59f1014ccc065fd50adbb4d284e932ffe86f768c
Vishal Biyani <vishal@infracloud.io>
2018-06-27 10:09:01 +0530

    Using a image only for build as a shorter way

--- test/tests/test_environments/test_java_env.sh
+++ test/tests/test_environments/test_java_env.sh
@@ -14,18 +14,14 @@ cd $ROOT/examples/jvm/java
 
 log "Creating the jar from application"
 #Using Docker to build Jar so that maven & other Java dependencies are not needed on CI server
-docker build -t javaex .
-docker run --name javabuilder javaex 
-docker cp javabuilder:/usr/src/myapp/target/hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar .
-docker rm -f javabuilder
-mv hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar app.jar
+docker run -it --rm  -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5-jdk-8 mvn clean package
 
 log "Creating environment for Java"
 fission env create --name jvm --image fission/jvm-env --version 2 --extract=false
 
 log "Creating pool manager & new deployment function for Java"
-fission fn create --name hellop --deploy app.jar --env jvm --entrypoint io.fission.HelloWorld
-fission fn create --name hellon --deploy app.jar --env jvm --executortype newdeploy --entrypoint io.fission.HelloWorld
+fission fn create --name hellop --deploy target/hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar --env jvm --entrypoint io.fission.HelloWorld
+fission fn create --name hellon --deploy target/hello-world-1.0-SNAPSHOT-jar-with-dependencies.jar --env jvm --executortype newdeploy --entrypoint io.fission.HelloWorld
 trap cleanup EXIT
 
 log "Waiting for router & pools to catch up"
@@ -35,9 +31,11 @@ log "Testing pool manager function"
 response=$(curl http://$FISSION_ROUTER/fission-function/hellop)
 
 log "Checking for valid response"
+echo $response
 echo $response | grep -i hello
 
 log "Testing new deployment function"
+echo $response
 response=$(curl http://$FISSION_ROUTER//fission-function/hellon)
 
 log "Checking for valid response"
