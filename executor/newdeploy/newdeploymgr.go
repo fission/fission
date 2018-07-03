fafef5c14b7b99e4011d33ce534d1db080ec2131
Ta-Ching Chen <contact@tachingchen.com>
2018-07-03 23:37:25 +0800

    Update comment

--- executor/newdeploy/newdeploymgr.go
+++ executor/newdeploy/newdeploymgr.go
@@ -585,7 +585,9 @@ func updateStatus(fn *crd.Function, err error, message string) {
 	log.Println(message, fn, err)
 }
 
-// IsValid does a get on the service address to ensure it's a valid service. returns true if it is, else false.
+// IsValid does a get on the service address to ensure it's a valid service, then
+// scale deployment to 1 replica if there are no available replicas for function.
+// Return true if no error occurs, return false otherwise.
 func (deploy *NewDeploy) IsValid(fsvc *fscache.FuncSvc) bool {
 	service := strings.Split(fsvc.Address, ".")
 	if len(service) == 0 {
