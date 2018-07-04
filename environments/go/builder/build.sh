0b9060385509b093e324fd5dcc54de8b6512fdae
Ta-Ching Chen <contact@tachingchen.com>
2018-07-04 17:20:57 +0800

    Add go environment vendor directory support

--- environments/go/builder/build.sh
+++ environments/go/builder/build.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
+srcDir=${GOPATH}/src/$(basename ${SRC_PKG})
+trap "rm -rf ${srcDir}" EXIT
+
 if [ -d ${SRC_PKG} ]
 then
-    echo "Building in directory ${SRC_PKG}"
-    cd ${SRC_PKG}
-    go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
+    cp -r ${SRC_PKG} ${srcDir}
+    echo "Building in directory ${srcDir}"
 elif [ -f ${SRC_PKG} ]
 then
-    fn=$(basename ${SRC_PKG})
-    d=/tmp/$fn
-    mkdir $d
-    trap "rm -rf /tmp/$fn" EXIT
-    echo "Building file ${SRC_PKG} in $d"
-
-    cd $d
-    cp ${SRC_PKG} ./function.go
-    go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
+    echo "Building file ${SRC_PKG} in ${srcDir}"
+    cp ${SRC_PKG} ${srcDir}/function.go
 fi
+
+cd ${srcDir}
+go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
