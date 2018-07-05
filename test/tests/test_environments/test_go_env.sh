c85fc1b7b42eca7e8153958ca324b60c3d8dcf17
Ta-Ching Chen <contact@tachingchen.com>
2018-07-06 01:14:49 +0800

    Wait for env builder ready

--- test/tests/test_environments/test_go_env.sh
+++ test/tests/test_environments/test_go_env.sh
@@ -10,6 +10,17 @@ cleanup() {
     fission env delete --name go || true
 }
 
+wait_for_builder() {
+    # wait for tiller ready
+    while true; do
+      kubectl --namespace fission-builder get pod -l envName=go|grep Running
+      if [[ $? -eq 0 ]]; then
+          break
+      fi
+      sleep 5
+    done
+}
+
 test_fn() {
     echo "Checking for valid response"
 
@@ -23,12 +34,15 @@ test_fn() {
     done
 }
 
+export -f wait_for_builder
 export -f test_fn
 
 cd $ROOT/examples/go
 
 log "Creating environment for Golang"
 fission env create --name go --image gcr.io/fission-ci/go-env:test --builder gcr.io/fission-ci/go-builder:test --period 5
+
+timeout 90 bash -c "wait_for_builder"
 
 log "Creating pool manager & new deployment function for Golang"
 fission fn create --name hello-go-poolmgr --env go --src hello.go --entrypoint Handler
