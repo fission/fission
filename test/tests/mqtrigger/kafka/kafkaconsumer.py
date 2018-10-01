d656e31c12faf2d4538abaf0c903c6624a20072f
Vishal Biyani <vishal@infracloud.io>
2018-10-01 08:04:35 +0530

    Removed tests from this change, will cover in a separate change

--- test/tests/mqtrigger/kafka/kafkaconsumer.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-from flask import current_app
-
-def main():
-    current_app.logger.info("Received request")
-    msg = "--BODY--\n%s\n-----\n" % (request.get_data())
-    return msg
