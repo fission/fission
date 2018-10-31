4e9e025ca45153c744dcd548a095da004e3b0a36
Soam Vasani <soamvasani@platform9.com>
2018-10-31 14:54:25 -0700

    Remove version from release name since it contains illegal chars for names (#939)
    * Remove version from release name since it contains illegal chars for names
    
    * Put version in release name for uniqueness, but remove invalid chars
    
    * Remove prometheus=false yaml

--- hack/release-build.sh
+++ hack/release-build.sh
@@ -235,14 +235,15 @@ build_yamls() {
     pushd ${DIR}/charts
     find . -iname *.~?~ | xargs -r rm
 
+    releaseName=fission-$(echo ${version} | sed 's/\./-/g')
+    
     for c in fission-all fission-core
     do
-        # for minikube and other environment not support ELB
-        helm template ${c} -n ${c}-${version} --set serviceType=NodePort,routerServiceType=NodePort > ${c}-${version}-minikube.yaml
-        # for environments support ELB
-        helm template ${c} -n ${c}-${version} > ${c}-${version}.yaml
-        # for cases where prometheus installation along with fission is not preferred
-        helm template ${c} -n ${c}-${version} --set canaryDeployment.prometheusDeploy=false > ${c}-${version}-fission-only.yaml
+        # for minikube and other environment that don't support LoadBalancer
+        helm template ${c} -n ${releaseName} --set serviceType=NodePort,routerServiceType=NodePort > ${c}-${version}-minikube.yaml
+        # for environments that support LoadBalancer
+        helm template ${c} -n ${releaseName} > ${c}-${version}.yaml
+
         mv *.yaml ${BUILDDIR}/yamls/
     done
 
