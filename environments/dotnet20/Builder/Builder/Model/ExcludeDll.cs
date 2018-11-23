3215872c9bef79474cb19340c6eef65c32a88247
APTTUS\ppatidar <ppatidar@apttus.com>
2018-11-23 13:47:56 +0530

    Fission dotnet 2.0 Builder image

---
+++ environments/dotnet20/Builder/Builder/Model/ExcludeDll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Model
+{
+    public class ExcludeDll
+    {
+        public string dllName { get; set; }
+        public string packageName { get; set; }
+    }
+}
