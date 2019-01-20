bf4b60a9eddfe4a8ca5197c9de31adc287ec65f6
Vishal Biyani <vishal@infracloud.io>
2019-01-20 16:15:49 +0530

    Adding flags for fixing JVM crash issue

--- environments/jvm/builder/build.sh
+++ environments/jvm/builder/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eou pipefail
-mvn clean package
+# Re - disableClassPathURLCheck flag: Latest Surefire plugin has bugs with JDK8 and causes VM to crash, check: https://stackoverflow.com/questions/23260057/the-forked-vm-terminated-without-saying-properly-goodbye-vm-crash-or-system-exi
+mvn clean package -DargLine="-Djdk.net.URLClassPath.disableClassPathURLCheck=true"
 cp ${SRC_PKG}/target/*with-dependencies.jar ${DEPLOY_PKG}
--- test/tests/test_environments/test_java_env.sh
+++ test/tests/test_environments/test_java_env.sh
@@ -29,7 +29,8 @@ cd $ROOT/examples/jvm/java
 
 log "Creating the jar from application"
 #Using Docker to build Jar so that maven & other Java dependencies are not needed on CI server
-docker run -it --rm  -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5-jdk-8 mvn clean package -q
+# Re - disableClassPathURLCheck flag: Latest Surefire plugin has bugs with JDK8 and causes VM to crash, check: https://stackoverflow.com/questions/23260057/the-forked-vm-terminated-without-saying-properly-goodbye-vm-crash-or-system-exi
+docker run -it --rm  -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5-jdk-8 mvn clean package -q -DargLine="-Djdk.net.URLClassPath.disableClassPathURLCheck=true"
 
 log "Creating environment for Java"
 fission env create --name jvm --image gcr.io/fission-ci/jvm-env:test --version 2 --keeparchive=true
