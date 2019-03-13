c717f182b69008f854abac5a9c39fd18e0c8ec40
Brendan Stennett <brendan.stennett@gmail.com>
2019-03-13 10:55:26 -0400

    Added support for Ruby v2 Specialization (#1101)
    * Added support for Ruby v2 Specialization
    * Added ruby v2 example with builder code

--- environments/ruby/server.rb
+++ environments/ruby/server.rb
@@ -1,21 +1,33 @@
 # frozen_string_literal: true
 
 require 'rack'
+require 'thin'
 require 'logger'
 
 require_relative 'fission/specializer'
 require_relative 'fission/handler'
 
+$handler = nil
+
 app = Rack::Builder.new do
   use Rack::Logger, Logger::DEBUG
+  use Rack::CommonLogger
 
   map "/specialize" do
     run Fission::Specializer
   end
 
-  map "/" do
+  map '/v2/specialize' do
+    run Fission::V2::Specializer
+  end
+
+  map "/healthz" do
+    run ->(env) { [ 200, {}, [] ] }
+  end
+
+  map '/' do
     run Fission::Handler
   end
 end
 
-Rack::Handler::WEBrick.run app, Host: '0.0.0.0', Port: 8888
+Rack::Handler::Thin.run app, Host: '0.0.0.0', Port: 8888
