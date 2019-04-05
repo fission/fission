bee735ef88767fdfeacd1db7f4f9cb944eea7ff9
Ta-Ching Chen <contact@tachingchen.com>
2019-04-06 02:41:32 +0800

    Enable go module support for go environment

--- environments/go/builder/build.sh
+++ environments/go/builder/build.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eux
 
 srcDir=${GOPATH}/src/$(basename ${SRC_PKG})
 
 trap "rm -rf ${srcDir}" EXIT
+
+# http://ask.xmodulo.com/compare-two-version-numbers.html
+version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
 if [ -d ${SRC_PKG} ]
 then
@@ -18,4 +21,14 @@ then
 fi
 
 cd ${srcDir}
+
+if [ -f "go.mod" ]; then
+    if [ ! -z ${GOLANG_VERSION} ] && version_ge ${GOLANG_VERSION} "1.11"; then
+        go mod download
+    else
+        echo "Please update fission/go-builder image to latest version to support go module"
+        exit 1
+    fi
+fi
+
 go build -buildmode=plugin -i -o ${DEPLOY_PKG} .
