4632269314d23dd4180b09a2d8f4ac4f81921cbc
Paras Patidar <paraspatidar@users.noreply.github.com>
2019-05-15 14:05:55 +0530

    Created dotnet2.0 Builder Image and Added /v2/specialized Endpoint to dotnet2.0 Envrionment  (#1001)

---
+++ environments/dotnet20/builder/Model/IncludeNuget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Model
+{
+    public class IncludeNuget
+    {
+        public string packageName { get; set; }
+        public string version { get; set; }
+    }
+}
