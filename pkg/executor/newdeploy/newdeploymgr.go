6c56d63be170c1ccf5a9f0266db324bda0583acf
Vishal Biyani <vishal@infracloud.io>
2019-07-09 15:12:30 +0530

    Minor fix

--- pkg/executor/newdeploy/newdeploymgr.go
+++ pkg/executor/newdeploy/newdeploymgr.go
@@ -33,6 +33,7 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
 	k8sErrs "k8s.io/apimachinery/pkg/api/errors"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
@@ -40,7 +41,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	k8sCache "k8s.io/client-go/tools/cache"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 
 	fv1 "github.com/fission/fission/pkg/apis/fission.io/v1"
 	"github.com/fission/fission/pkg/crd"
