8c36f61828e3ae435ecd4fb17a5ea8c09a5af856
Ta-Ching Chen <contact@tachingchen.com>
2019-11-21 03:39:06 +0800

    fix go fmt

--- pkg/fission-cli/cmd/function/test.go
+++ pkg/fission-cli/cmd/function/test.go
@@ -28,10 +28,10 @@ import (
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/fission/fission/pkg/fission-cli/console"
 	"github.com/fission/fission/pkg/controller/client"
 	"github.com/fission/fission/pkg/fission-cli/cliwrapper/cli"
 	"github.com/fission/fission/pkg/fission-cli/cmd/httptrigger"
+	"github.com/fission/fission/pkg/fission-cli/console"
 	flagkey "github.com/fission/fission/pkg/fission-cli/flag/key"
 	"github.com/fission/fission/pkg/fission-cli/util"
 )
