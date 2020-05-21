3947116d8b3552c01dd2a883d1f0a0c2e5f10708
therahulbhati <rjbhati009@gmail.com>
2020-05-21 10:01:29 +0530

    Updated reponse message

--- examples/spec-example/nodejs-bodyParser-limit-example/hello/hello.js
+++ examples/spec-example/nodejs-bodyParser-limit-example/hello/hello.js
@@ -1,9 +1,11 @@
 const process = require("process");
 
 module.exports = async function (context) {
-  let bodyParserLimit = process.env.BODY_PARSER_LIMIT || "1mb";
+  let message =
+    "BODY_PARSER_LIMIT received from env variable " +
+    process.env.BODY_PARSER_LIMIT;
   return {
     status: 200,
-    body: bodyParserLimit,
+    body: message,
   };
 };
