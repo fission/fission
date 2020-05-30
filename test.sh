1ed4f1cc0bf13d87a8cd76e046554a9e6dfdc79e
abhisheks-infracloud <abhishek@infracloud.io>
2020-05-30 20:44:24 +0530

    updated test script

--- test.sh
+++ test.sh
@@ -10,6 +10,7 @@ docker pull busybox
 docker tag busybox localhost:5000/busybox
 docker push localhost:5000/busybox
 kubectl delete -f pod.yaml --ignore-not-found
+kubectl create sa default -n fission
 n=0; until ((n >= 60)); do kubectl -n fission get serviceaccount default -o name && break; n=$((n + 1)); sleep 1; done; ((n < 60))
 kubectl create -f pod.yaml
 kubectl wait --for=condition=ready pod/kind-test --timeout=60s
