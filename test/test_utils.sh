7f0c317bf7ff1eb46d8e753ac5f584887ed76ac2
Vishal Biyani <vrbiyani@gmail.com>
2020-09-15 22:54:05 +0530

    Removing the fission-builder check for now

--- test/test_utils.sh
+++ test/test_utils.sh
@@ -238,10 +238,10 @@ helm_install_fission() {
     helm list -q|xargs -I@ bash -c "helm_uninstall_fission @"
 
     # deleting ns does take a while after command is issued
-    while kubectl get ns| grep "fission-builder"
-    do
-        sleep 5
-    done
+    # while kubectl get ns| grep "fission-builder"
+    # do
+    #     sleep 5
+    # done
 
     helm dependency update $ROOT/charts/fission-all
 
