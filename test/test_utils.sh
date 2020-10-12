584f89b86e81499a9fb8f1e15de25f8700734a58
therahulbhati <rjbhati009@gmail.com>
2020-10-12 19:50:00 +0530

    Added namespace and CRD deletion in build plan
    Signed-off-by: therahulbhati <rjbhati009@gmail.com>

--- test/test_utils.sh
+++ test/test_utils.sh
@@ -340,6 +340,10 @@ helm_uninstall_fission() {(set +e
     echo "Uninstalling fission"
     helm delete $id -n $ns || true
     kubectl delete ns f-$id || true
+    echo "Deleting CRDs"
+    kubectl get crd | grep "fission.io" | awk '{print $1}' | xargs -n1 kubectl delete crd
+    echo "Deleting Namespace"
+    kubectl delete ns $ns || true
 )}
 export -f helm_uninstall_fission
 
