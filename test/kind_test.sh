19e18cb2aa22514d6b95775f96124679ff7dc151
therahulbhati <rjbhati009@gmail.com>
2020-12-12 19:53:54 +0530

    return failures
    Signed-off-by: therahulbhati <rjbhati009@gmail.com>

--- test/kind_test.sh
+++ test/kind_test.sh
@@ -22,68 +22,73 @@ dump_system_info
 # export NODE_RUNTIME_IMAGE=fission/node-env-12.16:1.11.0
 # run tests without newdeploy in parallel.
 
+main() {
+    export JOBS=6
+    test/run_test.sh \
+        test/tests/test_canary.sh \
+        test/tests/test_fn_update/test_idle_objects_reaper.sh \
+        test/tests/mqtrigger/kafka/test_kafka.sh \
+        test/tests/test_annotations.sh \
+        test/tests/test_archive_pruner.sh \
+        test/tests/test_backend_poolmgr.sh \
+        test/tests/test_buildermgr.sh \
+        test/tests/test_env_vars.sh \
+        test/tests/test_environments/test_python_env.sh \
+        test/tests/test_function_test/test_fn_test.sh \
+        test/tests/test_function_update.sh \
+        test/tests/test_ingress.sh \
+        test/tests/test_internal_routes.sh \
+        test/tests/test_logging/test_function_logs.sh \
+        test/tests/test_node_hello_http.sh \
+        test/tests/test_package_command.sh \
+        test/tests/test_package_checksum.sh \
+        test/tests/test_pass.sh \
+        test/tests/test_specs/test_spec.sh \
+        test/tests/test_specs/test_spec_multifile.sh \
+        test/tests/test_specs/test_spec_merge/test_spec_merge.sh \
+        test/tests/test_specs/test_spec_archive/test_spec_archive.sh \
+        test/tests/test_environments/test_tensorflow_serving_env.sh \
+        test/tests/test_environments/test_go_env.sh \
+        test/tests/mqtrigger/nats/test_mqtrigger.sh \
+        test/tests/mqtrigger/nats/test_mqtrigger_error.sh \
+        test/tests/test_huge_response/test_huge_response.sh \
+        test/tests/test_kubectl/test_kubectl.sh
+    FAILURES=$?
 
-export JOBS=6
-test/run_test.sh \
-    test/tests/test_canary.sh \
-    test/tests/test_fn_update/test_idle_objects_reaper.sh \
-    test/tests/mqtrigger/kafka/test_kafka.sh \
-    test/tests/test_annotations.sh \
-    test/tests/test_archive_pruner.sh \
-    test/tests/test_backend_poolmgr.sh \
-    test/tests/test_buildermgr.sh \
-    test/tests/test_env_vars.sh \
-    test/tests/test_environments/test_python_env.sh \
-    test/tests/test_function_test/test_fn_test.sh \
-    test/tests/test_function_update.sh \
-    test/tests/test_ingress.sh \
-    test/tests/test_internal_routes.sh \
-    test/tests/test_logging/test_function_logs.sh \
-    test/tests/test_node_hello_http.sh \
-    test/tests/test_package_command.sh \
-    test/tests/test_package_checksum.sh \
-    test/tests/test_pass.sh \
-    test/tests/test_specs/test_spec.sh \
-    test/tests/test_specs/test_spec_multifile.sh \
-    test/tests/test_specs/test_spec_merge/test_spec_merge.sh \
-    test/tests/test_specs/test_spec_archive/test_spec_archive.sh \
-    test/tests/test_environments/test_tensorflow_serving_env.sh \
-    test/tests/test_environments/test_go_env.sh \
-    test/tests/mqtrigger/nats/test_mqtrigger.sh \
-    test/tests/mqtrigger/nats/test_mqtrigger_error.sh \
-    test/tests/test_huge_response/test_huge_response.sh \
-    test/tests/test_kubectl/test_kubectl.sh
-FAILURES=$?
+    export JOBS=3
+    test/run_test.sh \
+        test/tests/test_backend_newdeploy.sh \
+        test/tests/test_environments/test_java_builder.sh \
+        test/tests/test_environments/test_java_env.sh \
+        test/tests/test_environments/test_nodejs_env.sh \
+        test/tests/test_fn_update/test_configmap_update.sh \
+        test/tests/test_fn_update/test_env_update.sh \
+        test/tests/test_fn_update/test_nd_pkg_update.sh \
+        test/tests/test_fn_update/test_poolmgr_nd.sh \
+        test/tests/test_fn_update/test_resource_change.sh \
+        test/tests/test_fn_update/test_scale_change.sh \
+        test/tests/test_fn_update/test_secret_update.sh \
+        test/tests/test_obj_create_in_diff_ns.sh \
+        test/tests/test_secret_cfgmap/test_secret_cfgmap.sh
+    FAILURES=$((FAILURES+$?))
+    set -e
 
-export JOBS=3
-test/run_test.sh \
-    test/tests/test_backend_newdeploy.sh \
-    test/tests/test_environments/test_java_builder.sh \
-    test/tests/test_environments/test_java_env.sh \
-    test/tests/test_environments/test_nodejs_env.sh \
-    test/tests/test_fn_update/test_configmap_update.sh \
-    test/tests/test_fn_update/test_env_update.sh \
-    test/tests/test_fn_update/test_nd_pkg_update.sh \
-    test/tests/test_fn_update/test_poolmgr_nd.sh \
-    test/tests/test_fn_update/test_resource_change.sh \
-    test/tests/test_fn_update/test_scale_change.sh \
-    test/tests/test_fn_update/test_secret_update.sh \
-    test/tests/test_obj_create_in_diff_ns.sh \
-    test/tests/test_secret_cfgmap/test_secret_cfgmap.sh
-FAILURES=$((FAILURES+$?))
-set -e
+    # dump test logs
+    # TODO: the idx does not match seq number in recap.
+    idx=1
+    log_files=$(find test/logs/ -name '*.log')
 
-# dump test logs
-# TODO: the idx does not match seq number in recap.
-idx=1
-log_files=$(find test/logs/ -name '*.log')
+    for log_file in $log_files; do
+        test_name=${log_file#test/logs/}
+        # travis_fold_start run_test.$idx $test_name
+        echo "========== start $test_name =========="
+        cat $log_file
+        echo "========== end $test_name =========="
+        # travis_fold_end run_test.$idx
+        idx=$((idx+1))
+    done
 
-for log_file in $log_files; do
-    test_name=${log_file#test/logs/}
-    # travis_fold_start run_test.$idx $test_name
-    echo "========== start $test_name =========="
-    cat $log_file
-    echo "========== end $test_name =========="
-    # travis_fold_end run_test.$idx
-    idx=$((idx+1))
-done
+    return $FAILURES
+}
+
+main
