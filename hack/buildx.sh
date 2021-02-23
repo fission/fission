0c7c3352e8c0f27b56387396d07f13571adbc4f9
Harsh Thakur <harshthakur9030@gmail.com>
2021-02-23 21:05:34 +0530

    Comment on buildx
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

--- hack/buildx.sh
+++ hack/buildx.sh
@@ -1,3 +1,5 @@
+## This script needs to be run to start the emulator for multiarch builds
+
 docker run --rm --privileged linuxkit/binfmt:v0.8
 docker buildx create --use --name=qemu
 docker buildx inspect --bootstrap
