251f90dffaff1e62c6b1526885a0d083204a6fdc
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-26 00:27:13 +0530

    adding fission cli from soource

--- upgrade_test/upgrade_pre_req.sh
+++ upgrade_test/upgrade_pre_req.sh
@@ -3,8 +3,6 @@ set -ex
 
 #echo ".................Priting all Pods............................"
 kubectl get pods -A
-echo ".................installing fission CLI............................"
-curl -Lo fission https://github.com/fission/fission/releases/download/1.12.0/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/
 echo "....................Showing fission version......................."
 fission version
 #echo ".................Creating fission function............................"
@@ -13,4 +11,3 @@ fission version
 #fission function create --name hello --env nodejs --code hello.js
 #echo "...********************* Testing the Fission function **********************..."
 #fission function test --name hello
-        
