e8c7522d723d0840bb3dbd9d1247615db898163a
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-26 00:42:56 +0530

    correcting typo

--- upgrade_test/upgrade_pre_req.sh
+++ upgrade_test/upgrade_pre_req.sh
@@ -3,11 +3,3 @@ set -ex
 
 #echo ".................Priting all Pods............................"
 kubectl get pods -A
-echo "....................Showing fission version......................."
-fission version
-#echo ".................Creating fission function............................"
-#fission env create --name nodejs --image fission/node-env:latest
-#curl -LO https://raw.githubusercontent.com/fission/examples/master/nodejs/hello.js
-#fission function create --name hello --env nodejs --code hello.js
-#echo "...********************* Testing the Fission function **********************..."
-#fission function test --name hello
