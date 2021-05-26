1b52ca8186ff3faa7c600989a2a22885c94cd4e1
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-27 00:15:36 +0530

    script arrangment

--- upgrade_test/upgrade_pre_req.sh
+++ upgrade_test/upgrade_pre_req.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
 
-#echo ".................Priting all Pods............................"
-kubectl get pods --all-namespaces
+
+fission env create --name nodejs --image fission/node-env:latest
+curl -LO https://raw.githubusercontent.com/fission/examples/master/nodejs/hello.js
+fission function create --name hello --env nodejs --code hello.js
+sleep 30
+fission function test --name hello
