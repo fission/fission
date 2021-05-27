6725697291a38cf274ce51f430bbc0973edbfec6
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-27 08:03:25 +0530

    adding supporting scripts

--- upgrade_test/fission_cli.sh
+++ upgrade_test/fission_cli.sh
@@ -3,7 +3,7 @@ set -x
 
 
 setup_fission_cli() {
-      go build -ldflags "-X github.com/fission/fission/pkg/info.GitCommit=XTYGHJGHJG -X github.com/fission/fission/pkg/info.BuildDate=May-26-2021 -X github.com/fission/fission/pkg/info.Version=1.12" -o fission ../cmd/fission-cli/main.go
+      go build -ldflags "-X github.com/fission/fission/pkg/info.GitCommit=XTYGHJGHJG -X github.com/fission/fission/pkg/info.BuildDate=May-26-2021 -X github.com/fission/fission/pkg/info.Version=1.12" -o fission ./cmd/fission-cli/main.go
       sudo mv fission /usr/local/bin
       fission version
     
