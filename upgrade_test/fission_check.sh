a7babeb200fc4cb43c2e3b8014bb063240cc22d3
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-27 11:56:04 +0530

    script modifcation

--- upgrade_test/fission_check.sh
+++ upgrade_test/fission_check.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-set -ex
 
+flag=0
 
 setup_fission_function() {
+
     echo "==== Setting up objects for upgrade test ===="
-    echo "Creating env, function and route"
+    
     fission env create --name nodejs --image fission/node-env:latest
     curl -LO https://raw.githubusercontent.com/fission/examples/master/nodejs/hello.js
-    
     fission function create --name hello --env nodejs --code hello.js
-
     if [ $? == 0 ]
       then
       echo "Success, function created successfully !!!"
       else
       echo "Failure, received a non zero response"
+      exit
       fi
 
     sleep 2
 
-    echo "Executing function...."
+    echo "Testing function...."
     fission function test --name hello
-    
     if [ $? == 0 ]
       then
       echo "Success, function response received !!!"
       else
       echo "Failure, received a non zero response"
+      flag=1
       fi
-    
+
+    if [ $flag == 0 ]
+      then
+      echo "Both function creation and testing are successfull. Good to go :-)"
+      else
+      echo "Oops.. some failure occured :-("
+      fi
+
+
 }
 
 setup_fission_function
