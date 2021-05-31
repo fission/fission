c58c4f0a5c9fe18a8c3c7f43204e652923bc76ee
Jithindevasia <jithindevasia@Infraclouds-MacBook-Pro.local>
2021-05-31 08:30:57 +0530

    adding helm upgrade with dependency update

--- test/upgrade_test/install.sh
+++ test/upgrade_test/install.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 RANDOM=124
+ROOT=$(pwd)
 
 generate_test_id() {
     echo $(((10000 + $RANDOM) % 99999))
 }
 
-ROOT=$(pwd)
-#pushd $ROOT_RELPATH
-#ROOT=$(pwd)
-#popd
+
 
 # This will change for every new release
 CURRENT_VERSION=1.12.0
-
-#source /root/clone/fission/test/test_utils.sh
-#source /root/clone/fission//test/upgrade/fixture_tests.sh
 
 id=$(generate_test_id)
 ns=f-$id
@@ -26,8 +21,17 @@ routerServiceType=LoadBalancer
 
 helmVars=functionNamespace=$fns,controllerPort=$controllerNodeport,pullPolicy=Always,analytics=false,pruneInterval=$pruneInterval,routerServiceType=$routerServiceType
 
-#dump_system_info
 
+dump_system_info() {
+    travis_fold_start dump_system_info "System Info"
+    go version
+    docker version
+    kubectl version
+    helm version
+    
+    }
+
+dump_system_info
 
 echo "Deleting old releases"
 helm list -q|xargs -I@ bash -c "helm_uninstall_fission @"
@@ -38,55 +42,74 @@ do
     sleep 5
 done
 
-echo "Creating namespace $ns"
-kubectl create ns $ns
-helm install \
---namespace $ns \
---name-template fission \
-https://github.com/fission/fission/releases/download/${CURRENT_VERSION}/fission-all-${CURRENT_VERSION}.tgz
-
-mkdir temp && cd temp && curl -Lo fission https://github.com/fission/fission/releases/download/${CURRENT_VERSION}/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/ && cd .. && rm -rf temp
-sleep 60
-kubectl get pods -A
-fission env create --name nodejs --image fission/node-env:latest
-sleep 5
-curl -LO https://raw.githubusercontent.com/fission/examples/master/nodejs/hello.js
-fission function create --name hello --env nodejs --code hello.js
-sleep 5
-fission function test --name hello
 
 
-sleep 10
+install_stable_release () {
+    echo "Creating namespace $ns"
+    kubectl create ns $ns
+    helm install \
+    --namespace $ns \
+    --name-template fission \
+    https://github.com/fission/fission/releases/download/${CURRENT_VERSION}/fission-all-${CURRENT_VERSION}.tgz
 
-echo "Running new fission build..."
+    mkdir temp && cd temp && curl -Lo fission https://github.com/fission/fission/releases/download/${CURRENT_VERSION}/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/ && cd .. && rm -rf temp
+    sleep 60
+    kubectl get pods -A
+}
 
-docker build -t fission-bundle -f cmd/fission-bundle/Dockerfile.fission-bundle .
-docker build -t fetcher -f cmd/fetcher/Dockerfile.fission-fetcher .
-docker build -t builder -f cmd/builder/Dockerfile.fission-builder .
-docker build -t reporter -f cmd/reporter/Dockerfile.reporter .
+create_fission_objects () {
+    fission env create --name nodejs --image fission/node-env:latest
+    sleep 5
+    curl -LO https://raw.githubusercontent.com/fission/examples/master/nodejs/hello.js
+    fission function create --name hello --env nodejs --code hello.js
+    sleep 5
+    fission function test --name hello
+    sleep 10
+}
 
-sleep 5
 
-echo "fission helm upgrade....."
+build_docker_images () {
+    echo "Running new fission build..."
 
-IMAGE=fission-bundle
-FETCHER_IMAGE=fetcher
-BUILDER_IMAGE=builder
-TAG=latest
-PRUNE_INTERVAL=1 # Unit - Minutes; Controls the interval to run archivePruner.
-ROUTER_SERVICE_TYPE=ClusterIP
+    docker build -t fission-bundle -f cmd/fission-bundle/Dockerfile.fission-bundle .
+    docker build -t fetcher -f cmd/fetcher/Dockerfile.fission-fetcher .
+    docker build -t builder -f cmd/builder/Dockerfile.fission-builder .
+    docker build -t reporter -f cmd/reporter/Dockerfile.reporter .
 
-helmVars=analytics=false,pruneInterval=60,routerServiceType=LoadBalancer
+    sleep 5
+}
 
-echo "Upgrading fission"
 
-helm upgrade	\
- --timeout 540s	 \
- --set $helmVars \
- --namespace $ns  \
- fission \
- $ROOT/charts/fission-all
+install_current_release () {
+    
+    set -x
+    echo "Updating helm dependencies..."
+    helm dependency update $ROOT/charts/fission-all
+    
+    IMAGE=fission-bundle
+    FETCHER_IMAGE=fetcher
+    BUILDER_IMAGE=builder
+    TAG=latest
+    PRUNE_INTERVAL=1 # Unit - Minutes; Controls the interval to run archivePruner.
+    ROUTER_SERVICE_TYPE=ClusterIP
+    helmVars=analytics=false,pruneInterval=60,routerServiceType=LoadBalancer
 
-sleep 30
+    echo "Upgrading fission"
 
-kubectl get pods -A
+    helm upgrade	\
+    --timeout 540s	 \
+    --set $helmVars \
+    --namespace $ns  \
+    fission \
+    $ROOT/charts/fission-all
+
+    sleep 30
+    kubectl get pods -A
+
+}
+
+
+install_stable_release
+create_fission_objects
+build_docker_images
+install_current_release
