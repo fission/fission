7f9a18ec193646179feb822879d1498fa8efd388
Harsh Thakur <harshthakur9030@gmail.com>
2021-06-08 18:52:16 +0530

    Format, go version fix
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

--- test/tests/websocket/broadcast.js
+++ test/tests/websocket/broadcast.js
@@ -1,12 +1,11 @@
 module.exports = async function(ws, clients) {
    
-
-        ws.on('message', function incoming(data) {
-          clients.forEach(function each(client) {
+    ws.on('message', function incoming(data) {
+        clients.forEach(function each(client) {
               client.send(data);
           });
         });
-    
+
     ws.on('close', function close() {
         return {
             status: 200,
--- test/tests/websocket/go.mod
+++ test/tests/websocket/go.mod
@@ -1,5 +1,5 @@
 module ws
 
-go 1.14
+go 1.15
 
 require github.com/gorilla/websocket v1.4.2
