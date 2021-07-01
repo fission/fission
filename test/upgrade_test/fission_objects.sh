b326433e213dec25a4c729e96cac0afe3a43fbf5
Sanket Sudake <sanketsudake@gmail.com>
2021-07-01 12:20:35 +0530

    Dump all commands being executed
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- test/upgrade_test/fission_objects.sh
+++ test/upgrade_test/fission_objects.sh
@@ -6,32 +6,37 @@ ROOT=$(pwd)
 PREV_STABLE_VERSION=1.13.1
 HELM_VARS_LATEST_RELEASE="helmVars=repository=docker.io/library,image=fission-bundle,pullPolicy=IfNotPresent,imageTag=latest,fetcher.image=docker.io/library/fetcher,fetcher.imageTag=latest,postInstallReportImage=reporter,preUpgradeChecksImage=preupgradechecks"
 
+doit() {
+    echo "! $*"
+    "$@"
+}
+
 dump_system_info() {
     echo "System Info"
-    go version
-    docker version
-    kubectl version
-    helm version
+    doit go version
+    doit docker version
+    doit kubectl version
+    doit helm version
 }
 
 install_stable_release() {
-    helm repo add fission-charts https://fission.github.io/fission-charts/
-    helm repo update
+    doit helm repo add fission-charts https://fission.github.io/fission-charts/
+    doit helm repo update
 
     echo "Creating namespace $ns"
-    kubectl create ns $ns
+    doit kubectl create ns $ns
 
     echo "Creating CRDs"
-    kubectl create -k "github.com/fission/fission/crds/v1?ref=$PREV_STABLE_VERSION"
+    doit kubectl create -k "github.com/fission/fission/crds/v1?ref=$PREV_STABLE_VERSION"
 
     echo "Installing Fission $PREV_STABLE_VERSION"
-    helm install --version $PREV_STABLE_VERSION--namespace $ns fission fission-charts/fission-all
+    doit helm install --version $PREV_STABLE_VERSION --namespace $ns fission fission-charts/fission-all
 
     echo "Download fission cli $PREV_STABLE_VERSION"
     curl -Lo fission https://github.com/fission/fission/releases/download/$PREV_STABLE_VERSION/fission-$PREV_STABLE_VERSION-linux-amd64 && chmod +x fission && sudo mv fission /usr/local/bin/
     sleep 10
 
-    fission version
+    doit fission version
 }
 
 create_fission_objects() {
@@ -72,32 +77,32 @@ test_fission_objects() {
 
 build_docker_images() {
     echo "Building new docker images"
-    docker build -t fission-bundle -f cmd/fission-bundle/Dockerfile.fission-bundle .
-    docker build -t fetcher -f cmd/fetcher/Dockerfile.fission-fetcher .
-    docker build -t builder -f cmd/builder/Dockerfile.fission-builder .
-    docker build -t reporter -f cmd/reporter/Dockerfile.reporter .
-    docker build -t preupgradechecks -f cmd/preupgradechecks/Dockerfile.fission-preupgradechecks .
+    doit docker build -t fission-bundle -f cmd/fission-bundle/Dockerfile.fission-bundle .
+    doit docker build -t fetcher -f cmd/fetcher/Dockerfile.fission-fetcher .
+    doit docker build -t builder -f cmd/builder/Dockerfile.fission-builder .
+    doit docker build -t reporter -f cmd/reporter/Dockerfile.reporter .
+    doit docker build -t preupgradechecks -f cmd/preupgradechecks/Dockerfile.fission-preupgradechecks .
 }
 
 kind_image_load() {
     echo "Loading Docker images into Kind cluster."
-    kind load docker-image fission-bundle --name kind
-    kind load docker-image fetcher --name kind
-    kind load docker-image builder --name kind
-    kind load docker-image reporter --name kind
-    kind load docker-image preupgradechecks --name kind
+    doit kind load docker-image fission-bundle --name kind
+    doit kind load docker-image fetcher --name kind
+    doit kind load docker-image builder --name kind
+    doit kind load docker-image reporter --name kind
+    doit kind load docker-image preupgradechecks --name kind
 }
 
 install_fission_cli() {
     echo "Installing new Fission cli"
-    make install-fission-cli
+    doit make install-fission-cli
 }
 
 install_current_release() {
     echo "Running Fission upgrade"
-    helm dependency update "$ROOT"/charts/fission-all
-    make update-crds
-    helm upgrade --namespace $ns --set $HELM_VARS_LATEST_RELEASE fission "$ROOT"/charts/fission-all
+    doit helm dependency update "$ROOT"/charts/fission-all
+    doit make update-crds
+    doit helm upgrade --namespace $ns --set $HELM_VARS_LATEST_RELEASE fission "$ROOT"/charts/fission-all
 }
 
 "$@"
