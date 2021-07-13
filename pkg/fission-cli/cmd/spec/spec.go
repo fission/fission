b728029f1c47411e91502111e0a00d6e239853e6
Sanket Sudake <sanketsudake@gmail.com>
2021-07-13 16:00:26 +0530

    Changes for spec apply
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- pkg/fission-cli/cmd/spec/spec.go
+++ pkg/fission-cli/cmd/spec/spec.go
@@ -361,38 +361,40 @@ func (fr *FissionResources) Validate(input cli.Input) ([]string, error) {
 	for _, f := range fr.Functions {
 		functions[MapKey(&f.ObjectMeta)] = false
 
-		pkgMeta := &metav1.ObjectMeta{
-			Name:      f.Spec.Package.PackageRef.Name,
-			Namespace: f.Spec.Package.PackageRef.Namespace,
-		}
+		if f.Spec.InvokeStrategy.ExecutionStrategy.ExecutorType != fv1.ExecutorTypeContainer {
+			pkgMeta := &metav1.ObjectMeta{
+				Name:      f.Spec.Package.PackageRef.Name,
+				Namespace: f.Spec.Package.PackageRef.Namespace,
+			}
 
-		// check package ref from function
-		packageRefExists := func() bool {
-			_, ok := packages[MapKey(pkgMeta)]
-			return ok
-		}
+			// check package ref from function
+			packageRefExists := func() bool {
+				_, ok := packages[MapKey(pkgMeta)]
+				return ok
+			}
 
-		// check that the package referenced by each function is in the same ns as the function
-		packageRefInFuncNs := func(f *fv1.Function) bool {
-			return f.Spec.Package.PackageRef.Namespace == f.ObjectMeta.Namespace
-		}
+			// check that the package referenced by each function is in the same ns as the function
+			packageRefInFuncNs := func(f *fv1.Function) bool {
+				return f.Spec.Package.PackageRef.Namespace == f.ObjectMeta.Namespace
+			}
 
-		if !packageRefInFuncNs(&f) {
-			result = multierror.Append(result, fmt.Errorf(
-				"%v: function '%v' references a package outside of its namespace %v/%v",
-				fr.SourceMap.Locations["Function"][f.ObjectMeta.Namespace][f.ObjectMeta.Name],
-				f.ObjectMeta.Name,
-				f.Spec.Package.PackageRef.Namespace,
-				f.Spec.Package.PackageRef.Name))
-		} else if !packageRefExists() {
-			result = multierror.Append(result, fmt.Errorf(
-				"%v: function '%v' references unknown package %v/%v",
-				fr.SourceMap.Locations["Function"][f.ObjectMeta.Namespace][f.ObjectMeta.Name],
-				f.ObjectMeta.Name,
-				pkgMeta.Namespace,
-				pkgMeta.Name))
-		} else {
-			packages[MapKey(pkgMeta)] = true
+			if !packageRefInFuncNs(&f) {
+				result = multierror.Append(result, fmt.Errorf(
+					"%v: function '%v' references a package outside of its namespace %v/%v",
+					fr.SourceMap.Locations["Function"][f.ObjectMeta.Namespace][f.ObjectMeta.Name],
+					f.ObjectMeta.Name,
+					f.Spec.Package.PackageRef.Namespace,
+					f.Spec.Package.PackageRef.Name))
+			} else if !packageRefExists() {
+				result = multierror.Append(result, fmt.Errorf(
+					"%v: function '%v' references unknown package %v/%v",
+					fr.SourceMap.Locations["Function"][f.ObjectMeta.Namespace][f.ObjectMeta.Name],
+					f.ObjectMeta.Name,
+					pkgMeta.Namespace,
+					pkgMeta.Name))
+			} else {
+				packages[MapKey(pkgMeta)] = true
+			}
 		}
 
 		client, err := util.GetServer(input)
