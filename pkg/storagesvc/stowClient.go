1d22b7596c864cab5dfb927aac86c1f5813fee8b
Ankit Chawla <ankitc226@gmail.com>
2022-02-21 15:34:38 +0530

    Revert "Added function to sanitize strings by escaping quotes (#2360)" (#2367)
    This reverts commit fe5e5f592b253930bf7c4994e2c0a2d6298f8aed.

--- pkg/storagesvc/stowClient.go
+++ pkg/storagesvc/stowClient.go
@@ -27,8 +27,6 @@ import (
 	"github.com/graymeta/stow"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
-
-	"github.com/fission/fission/pkg/utils"
 )
 
 type (
@@ -175,7 +173,6 @@ func (client *StowClient) copyFileToStream(fileId string, w io.Writer) error {
 		return ErrWritingFileIntoResponse
 	}
 
-	fileId = utils.EscapeQuotes(fileId)
 	client.logger.Debug("successfully wrote file into httpresponse", zap.String("file", fileId))
 	return nil
 }
