e4751d54cb2ab58bccaf96bf9a2bfecdf8bf816e
Shaunak Deshmukh <kanuahs@gmail.com>
2022-09-02 13:40:49 +0530

    Add dashboard linting workflow (#2529)
    Co-authored-by: shaunak_deshmukh <shaunak@infracloud.io>

---
+++ hack/lint-dashboards.sh
@@ -0,0 +1,16 @@
+if ! command -v dashboard-linter >/dev/null 2>&1; then
+    echo "dashboard-linter is not installed"
+    exit 1;
+fi
+BASE_PATH=$(pwd)
+
+if [[ -z "$BASE_PATH" ]] ; then
+    BASE_PATH=$(GITHUB_WORKSPACE)
+fi
+
+DASHBOARD_PATH="$BASE_PATH/charts/fission-all/dashboards/*"
+
+for f in $DASHBOARD_PATH
+do
+    dashboard-linter lint --strict $f
+done
