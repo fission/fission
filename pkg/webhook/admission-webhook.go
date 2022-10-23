a5fc9a3de230ad059cdf6d3e5a0e950eecd638be
Neha Gupta <nehagupta161995@gmail.com>
2022-10-23 13:38:33 +0530

    helm charts for webhook

--- pkg/webhook/admission-webhook.go
+++ pkg/webhook/admission-webhook.go
@@ -18,65 +18,60 @@ package webhook
 
 import (
 	"context"
-	"flag"
 	"log"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 
 	v1 "github.com/fission/fission/pkg/apis/core/v1"
+	"github.com/fission/fission/pkg/generated/clientset/versioned/scheme"
 	"go.uber.org/zap"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	optzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 	//+kubebuilder:scaffold:imports
 )
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
+// var (
+// 	scheme   = runtime.NewScheme()
+// 	setupLog = ctrl.Log.WithName("setup")
+// )
 
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+// func init() {
+// 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	//+kubebuilder:scaffold:scheme
-}
+// 	//+kubebuilder:scaffold:scheme
+// }
 
 func Start(ctx context.Context, logger *zap.Logger, port int) (err error) {
 
 	wLogger := logger.Named("webhook")
 	// var params HookParamters
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := optzap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
+	// var metricsAddr string
+	// var enableLeaderElection bool
+	// var probeAddr string
+	// flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	// flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	// flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+	// 	"Enable leader election for controller manager. "+
+	// 		"Enabling this will ensure there is only one active controller manager.")
+	// opts := optzap.Options{
+	// 	Development: true,
+	// }
+	// opts.BindFlags(flag.CommandLine)
+	// flag.Parse()
 
 	// Setup a Manager
 	log.Println("setting up manager")
 	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   port, // TODO: implement this
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "1e55c7b8.fission.io",
+		Scheme: scheme.Scheme,
+		// MetricsBindAddress: metricsAddr,
+		Port: port, // TODO: implement this
+		// HealthProbeBindAddress: probeAddr,
+		// LeaderElection:         enableLeaderElection,
+		// LeaderElectionID:       "1e55c7b8.fission.io",
 	})
 	if err != nil {
 		wLogger.Error("unable to set up overall controller manager", zap.Error(err))
@@ -96,58 +91,10 @@ func Start(ctx context.Context, logger *zap.Logger, port int) (err error) {
 		return err
 	}
 
-	setupLog.Info("starting manager")
+	wLogger.Info("starting manager")
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
 		wLogger.Error("unable to run manager", zap.Error(err))
 		return err
 	}
-	// // Setup webhooks
-	// setupLog.Info("setting up webhook server")
-	// hookServer := mgr.GetWebhookServer()
-	// // hookServer.Start()
-
-	// hookServer.Port = params.port
-	// hookServer.CertDir = params.certDir
-
-	// setupLog.Info("registering webhooks to the webhook server")
-
-	// // hookServer.Register("/mutate-v1-pod", &webhook.Admission{Handler: &podAnnotator{Client: mgr.GetClient()}})
-	// hookServer.Register("environments/v1/validate", &webhook.Admission{Handler: &EnvValidator{Client: mgr.GetClient()}})
-
 	return nil
 }
-
-// // EnvValidator adds an annotation to every incoming pods.
-// func (v *EnvValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-// 	env := &corev1.Environment{}
-
-// 	err := v.decoder.Decode(req, env)
-// 	if err != nil {
-// 		return admission.Errored(http.StatusBadRequest, err)
-// 	}
-
-// 	err = env.Validate()
-// 	if err != nil {
-// 		return admission.Denied(fmt.Sprintf("could not validate due to  %s", err))
-// 	}
-
-// 	return admission.Allowed("")
-// }
-
-// // envAnnotator implements inject.Client.
-// // A client will be automatically injected.
-
-// // InjectClient injects the client.
-// func (a *EnvValidator) InjectClient(c client.Client) error {
-// 	a.Client = c
-// 	return nil
-// }
-
-// // podAnnotator implements admission.DecoderInjector.
-// // A decoder will be automatically injected.
-
-// // InjectDecoder injects the decoder.
-// func (a *EnvValidator) InjectDecoder(d *admission.Decoder) error {
-// 	a.decoder = d
-// 	return nil
-// }
