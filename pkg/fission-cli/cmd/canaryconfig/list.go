6785298675af3ea193140d99b030a2561ace7814
Sanket Sudake <sanketsudake@gmail.com>
2022-11-03 15:51:18 +0530

    Fix imports
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- pkg/fission-cli/cmd/canaryconfig/list.go
+++ pkg/fission-cli/cmd/canaryconfig/list.go
@@ -22,13 +22,13 @@ import (
 	"text/tabwriter"
 
 	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "github.com/fission/fission/pkg/apis/core/v1"
 	"github.com/fission/fission/pkg/fission-cli/cliwrapper/cli"
 	"github.com/fission/fission/pkg/fission-cli/cmd"
 	flagkey "github.com/fission/fission/pkg/fission-cli/flag/key"
 	"github.com/fission/fission/pkg/fission-cli/util"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ListSubCommand struct {
--- pkg/fission-cli/cmd/package/list.go
+++ pkg/fission-cli/cmd/package/list.go
@@ -24,13 +24,13 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	fv1 "github.com/fission/fission/pkg/apis/core/v1"
 	"github.com/fission/fission/pkg/fission-cli/cliwrapper/cli"
 	"github.com/fission/fission/pkg/fission-cli/cmd"
 	flagkey "github.com/fission/fission/pkg/fission-cli/flag/key"
 	"github.com/fission/fission/pkg/fission-cli/util"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ListSubCommand struct {
