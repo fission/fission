d362300e8972c0a00a9e6247b1fba67a456c22df
Sanket Sudake <sanketsudake@gmail.com>
2022-12-15 12:19:14 +0530

    Allow service account check to run only once at start of executor
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

---
+++ pkg/utils/serviceaccount_test.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"context"
+	"os"
+	"regexp"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+
+	"github.com/fission/fission/pkg/utils/loggerfactory"
+)
+
+func TestServiceAccountCheck(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	kubernetesClient := fake.NewSimpleClientset()
+	logger := loggerfactory.GetLogger()
+	os.Setenv(ENV_CREATE_SA, "true")
+	CreateMissingPermissionForSA(ctx, kubernetesClient, logger)
+
+	// Get rolebinding for a service account
+	rolebindings, err := kubernetesClient.RbacV1().RoleBindings("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(rolebindings.Items) != 2 {
+		t.Fatal("Rolebinding not created", len(rolebindings.Items))
+	}
+	regexp := regexp.MustCompile(`fission\-(fetcher|builder)\-rolebinding\-[a-z0-9]{6}`)
+	for _, rolebinding := range rolebindings.Items {
+		if !regexp.Match([]byte(rolebinding.Name)) {
+			t.Fatal("Rolebinding not created for fission-builder or fission-fetcher", rolebinding.Name)
+		}
+	}
+}
