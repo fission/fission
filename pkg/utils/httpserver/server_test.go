9a5ce05c06204a25df69f68364d904683d3b7bd4
Sanket Sudake <sanketsudake@gmail.com>
2023-10-24 22:37:00 +0530

    Update server test
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- pkg/utils/httpserver/server_test.go
+++ pkg/utils/httpserver/server_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
 	"github.com/fission/fission/pkg/utils/loggerfactory"
@@ -27,36 +28,33 @@ func TestStartServer(t *testing.T) {
 	go StartServer(ctx, logger, "test", "8999", m)
 
 	tests := []struct {
+		Name       string
 		URL        string
 		StatusCode int
 		Body       string
 	}{
 		{
+			Name:       "test handler",
 			URL:        "http://localhost:8999",
 			StatusCode: http.StatusOK,
 			Body:       "test handler",
 		},
 		{
+			Name:       "not found",
 			URL:        "http://localhost:8999/notfound",
 			StatusCode: http.StatusNotFound,
 			Body:       "404 page not found\n",
 		},
 	}
 	for _, test := range tests {
-		resp, err := http.Get(test.URL)
-		if err != nil {
-			t.Errorf("failed to make get request %v: %v", test.URL, err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != test.StatusCode {
-			t.Errorf("expected status code %v, got %v", test.StatusCode, resp.StatusCode)
-		}
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("failed to read response body: %v", err)
-		}
-		if string(body) != test.Body {
-			t.Errorf("expected body \"%v\", got \"%v\"", test.Body, string(body))
-		}
+		t.Run(test.Name, func(t *testing.T) {
+			resp, err := http.Get(test.URL)
+			require.NoError(t, err, "failed to make get request %s", test.URL)
+			defer resp.Body.Close()
+			require.Equal(t, test.StatusCode, resp.StatusCode)
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.Equal(t, string(body), test.Body)
+		})
 	}
 }
