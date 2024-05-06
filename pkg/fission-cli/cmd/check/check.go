7c75209eee7a568c666fcb99188241e2a5128665
Md Soharab Ansari <soharab.ansari@infracloud.io>
2024-05-06 15:35:13 +0530

    Resolve review comments
    Signed-off-by: Md Soharab Ansari <soharab.ansari@infracloud.io>

--- pkg/fission-cli/cmd/check/check.go
+++ pkg/fission-cli/cmd/check/check.go
@@ -14,11 +14,12 @@ limitations under the License.
 package check
 
 import (
+	"fmt"
+
 	"github.com/fission/fission/pkg/fission-cli/cliwrapper/cli"
 	"github.com/fission/fission/pkg/fission-cli/cmd"
 	flagkey "github.com/fission/fission/pkg/fission-cli/flag/key"
 	"github.com/fission/fission/pkg/healthcheck"
-	"github.com/pkg/errors"
 )
 
 type CheckSubCommand struct {
@@ -33,9 +34,9 @@ func (opts *CheckSubCommand) do(input cli.Input) error {
 
 	checks := []healthcheck.CategoryID{}
 
-	userProvidedNS, _, err := opts.GetResourceNamespace(input, flagkey.NamespaceFunction)
+	userProvidedNS, _, err := opts.GetResourceNamespace(input, flagkey.Namespace)
 	if err != nil {
-		return errors.Wrap(err, "error retrieving user provided namespace information")
+		return fmt.Errorf("error retrieving user provided namespace information: %w", err)
 	}
 
 	if input.IsSet(flagkey.PreCheckOnly) {
