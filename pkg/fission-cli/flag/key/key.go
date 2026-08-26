2463d14b9f25009ce3425c2ed0fdd50a0037c061
Sanket Sudake <sanketsudake@gmail.com>
2026-08-26 18:03:11 +0530

    feat(cli): fission env --runtime-class flag

--- pkg/fission-cli/flag/key/key.go
+++ pkg/fission-cli/flag/key/key.go
@@ -226,6 +226,7 @@ const (
 	EnvForce           = force
 	EnvBuilder         = "builder-env"
 	EnvRuntime         = "runtime-env"
+	EnvRuntimeClass    = "runtimeclass"
 
 	KwName      = resourceName
 	KwFnName    = "function"
